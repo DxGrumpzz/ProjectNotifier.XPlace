ad99592f8bbede6cbc520978a68f7349d73727fe
Josef Lintz <yosi1.lints@gmail.com>
2019-08-10 19:12:07 +0200

    + Added ProjectListView a view that is responsible for displaying a list of projects

---
+++ XPlace-ProjectNotifier/Viewmodels/Project ViewModel/ProjectListViewModel.cs
@@ -0,0 +1,92 @@
+﻿namespace XPlace_ProjectNotifier
+{
+    using System;
+    using System.Collections.ObjectModel;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ProjectListViewModel : BaseViewModel
+	{
+
+		public static ProjectListViewModel DesignTimeInstance => new ProjectListViewModel()
+		{
+			ProjectList = new ObservableCollection<ProjectItemViewModel>()
+			{
+				new ProjectItemViewModel()
+				{
+					ProjectModel = new ProjectModel()
+					{
+						Title = "Project title",
+
+						PublishingDate = DateTime.Now,
+
+						Link = "project link",
+
+						Description = "Project descritpion",
+					},
+				},
+				new ProjectItemViewModel()
+				{
+					ProjectModel = new ProjectModel()
+					{
+						Title = "Project title",
+
+						PublishingDate = DateTime.Now,
+
+						Link = "project link",
+
+						Description = "Project descritpion",
+					},
+				},
+				new ProjectItemViewModel()
+				{
+					ProjectModel = new ProjectModel()
+					{
+						Title = "Project title",
+
+						PublishingDate = DateTime.Now,
+
+						Link = "project link",
+
+						Description = "Project descritpion",
+					},
+				},
+				new ProjectItemViewModel()
+				{
+					ProjectModel = new ProjectModel()
+					{
+						Title = "Project title",
+
+						PublishingDate = DateTime.Now,
+
+						Link = "project link",
+
+						Description = "Project descritpion",
+					},
+				},
+			},
+		};
+
+		#region Private fields
+
+		private ObservableCollection<ProjectItemViewModel> _projectList;
+
+		#endregion
+
+
+		/// <summary>
+		/// A List of projects
+		/// </summary>
+		public ObservableCollection<ProjectItemViewModel> ProjectList
+		{
+			get => _projectList;
+			set
+			{
+				_projectList = value;
+				OnPoropertyChanged();
+			}
+		}
+
+	};
+}
---
+++ XPlace-ProjectNotifier/Views/Project Views/ProjectListView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for ProjectListView.xaml
+	/// </summary>
+	public partial class ProjectListView : UserControl
+	{
+		public ProjectListView()
+		{
+			InitializeComponent();
+		}
+	};
+}
