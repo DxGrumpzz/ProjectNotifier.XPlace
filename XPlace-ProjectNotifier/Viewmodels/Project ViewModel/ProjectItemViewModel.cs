c8153fbdb1c02b7891f4877508d5f7daaef4c248
Josef Lintz <yosi1.lints@gmail.com>
2019-08-10 18:14:53 +0200

    * Fixed some design time weirdness

--- XPlace-ProjectNotifier/Viewmodels/Project ViewModel/ProjectItemViewModel.cs
+++ XPlace-ProjectNotifier/Viewmodels/Project ViewModel/ProjectItemViewModel.cs
@@ -2,17 +2,55 @@
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Diagnostics;
 	using System.Text;
+	using System.Windows;
 
 	/// <summary>
 	/// 
 	/// </summary>
 	public class ProjectItemViewModel : BaseViewModel
 	{
+		public static ProjectItemViewModel DesignTimeInstance => new ProjectItemViewModel()
+		{
+			ProjectModel = new ProjectModel()
+			{
+				Title = "Project title",
+
+				PublishingDate = DateTime.Now,
+
+				Link = "project link",
+
+				Description = "Project descritpion",
+			},
+		};
+
+
+
+		#region Private fields
+
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		private ProjectModel _projectModel;
+
+		#endregion
+
 		/// <summary>
 		/// A model containing data about the project
 		/// </summary>
-		public ProjectModel ProjectModel { get; set; }
+		public ProjectModel ProjectModel
+		{
+			get => _projectModel;
+			set
+			{
+				_projectModel = value;
+				OnPoropertyChanged();
+			}
+		}
 
+
+		public ProjectItemViewModel()
+		{
+
+		}
 	};
 }
