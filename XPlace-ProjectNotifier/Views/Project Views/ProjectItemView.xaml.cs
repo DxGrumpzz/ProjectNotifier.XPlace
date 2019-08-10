ce6fa1ee8e10adc1578e6e4414785597a0b00440
Josef Lintz <yosi1.lints@gmail.com>
2019-08-10 17:12:21 +0200

    + Added project item View

---
+++ XPlace-ProjectNotifier/Views/Project Views/ProjectItemView.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+	using System.ComponentModel;
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for ProjectItemView.xaml
+	/// </summary>
+	public partial class ProjectItemView : UserControl
+	{
+
+		/// <summary>
+		/// Desgin time view model
+		/// </summary>
+		public ProjectItemViewModel designTimeViewModel = new ProjectItemViewModel()
+		{
+			ProjectModel = new ProjectModel()
+			{
+				Title = "Project title",
+
+				PublishingDate = DateTime.Now,
+
+
+				Link = "project link",
+				Description = "Project descritpion",
+			},
+		};
+
+		public ProjectItemView()
+		{
+			// If control is in design mode, Set desgin time viewtmodel
+			if (DesignerProperties.GetIsInDesignMode(this))
+			{
+				DataContext = designTimeViewModel;
+			};
+
+			InitializeComponent();
+		}
+
+
+		public ProjectItemView(ProjectItemViewModel viewModel) :
+			this()
+		{
+			DataContext = viewModel;
+		}
+	};
+}
