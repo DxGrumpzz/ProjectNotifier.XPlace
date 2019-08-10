899f856d21052ca63bd3244e86431e32c1707f87
Josef Lintz <yosi1.lints@gmail.com>
2019-08-10 15:59:51 +0200

    + Added RssReader a class that is responsible for consuming an RSS feed and doing stuff to it

---
+++ XPlace-ProjectNotifier/RSSReader/RSSReader.cs
@@ -0,0 +1,45 @@
+﻿namespace XPlace_ProjectNotifier
+{
+    using System.Xml;
+    using System.Xml.Linq;
+	using System.Linq;
+
+    /// <summary>
+    /// An RSS reader/prases
+    /// </summary>
+    public class RSSReader
+	{
+		#region Private fields
+
+		/// <summary>
+		/// The url to the Xml document
+		/// </summary>
+		string _rssXmlUrl;
+
+		XmlDocument _rssDocumnet;
+
+		#endregion
+
+		public RSSReader(string rssUrl)
+		{
+			_rssDocumnet = new XmlDocument();
+			_rssDocumnet.Load(rssUrl);
+
+			_rssXmlUrl = rssUrl;
+		}
+
+
+		/// <summary>
+		/// Returns an <see cref="XmlNodeList"/> that contains information about items in the document
+		/// </summary>
+		/// <param name="nodesToSelect"> node selection "filter" </param>
+		public XmlNodeList GetNodeFromRssDocument(string nodesToSelect = "rss/channel/item")
+		{
+			// Select XML nodes from the RSS document
+			XmlNodeList rssNodes = _rssDocumnet.SelectNodes(nodesToSelect);
+
+			return rssNodes;
+		}
+
+	}
+}
