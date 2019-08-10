4710c4536ec87b7b2eedaa76f15c3f722be1f59d
Josef Lintz <yosi1.lints@gmail.com>
2019-08-10 17:11:57 +0200

    + Added Project item viewmodel

---
+++ XPlace-ProjectNotifier/Viewmodels/Project ViewModel/ProjectItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ProjectItemViewModel : BaseViewModel
+	{
+		/// <summary>
+		/// A model containing data about the project
+		/// </summary>
+		public ProjectModel ProjectModel { get; set; }
+
+	};
+}
