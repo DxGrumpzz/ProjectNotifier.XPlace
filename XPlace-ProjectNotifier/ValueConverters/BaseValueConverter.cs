329e1f4fc79e08f0cd84870a878bc2a929f893f6
Josef Lintz <yosi1.lints@gmail.com>
2019-08-11 09:09:14 +0200

    + Added a base ValueConverter that all ValueConverters will inherit from

---
+++ XPlace-ProjectNotifier/ValueConverters/BaseValueConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Markup;
+
+namespace MuLauncher
+{
+    /// <summary>
+    /// A base ValueConverter for all <see cref="IValueConverter"/> classes to inherit.
+	/// Allows for a more simple implmentation in Xaml
+    /// </summary>
+    /// <typeparam name="T"> The type of class or ValueConverter </typeparam>
+    abstract public class BaseValueConverter<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+
+        /// <summary>
+        /// A single static instance of this value converter
+        /// </summary>
+        private static T Converter = null;
+
+
+        /// <summary>
+        /// Converts a value into another value with a possibility to pass a specifc value with the parameter
+        /// </summary>
+        /// <param name="value"> The value to convert </param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"> </param>
+        /// <param name="culture"></param>
+        /// <returns> Returns an <see cref="object"/> that can be cast into any other object </returns>
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+
+        /// <summary>
+        /// Provides a static instance of the value converter 
+        /// </summary>
+        /// <param name="serviceProvider">The service provider</param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            // If the converter instance is null create a new type of what ever type passed in the type argument
+            return Converter ?? (Converter = new T());
+        }
+    }
+}
