159499e79c8dd71d4a95183aee78fbfacfa8893b
Josef Lintz <yosi1.lints@gmail.com>
2019-08-11 09:14:39 +0200

    + Added an incremental value converter. Takes a count and returns it "+parameter"

---
+++ XPlace-ProjectNotifier/ValueConverters/IncrementNumberValueConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+
+
+	/// <summary>
+	/// Increments an index or a count by some number
+	/// </summary>
+	public class IncrementNumberValueConverter : BaseValueConverter<IncrementNumberValueConverter>
+	{
+		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (int)value + System.Convert.ToInt32(parameter);
+		}
+
+		public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	};
+}
