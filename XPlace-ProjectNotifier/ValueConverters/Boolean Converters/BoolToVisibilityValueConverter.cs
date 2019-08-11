c3198e17abb575931ba1a32cd4efb413a6c1294e
Josef Lintz <yosi1.lints@gmail.com>
2019-08-11 13:49:53 +0200

    + Added a Value converter that takes a boolean value and returns a Visibility

---
+++ XPlace-ProjectNotifier/ValueConverters/Boolean Converters/BoolToVisibilityValueConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+    using System.Windows;
+
+
+    /// <summary>
+    /// Convert a Boolean value to visibility 
+    /// </summary>
+    public class BoolToVisibilityValueConverter : BaseValueConverter<BoolToVisibilityValueConverter>
+	{
+		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (bool)value == true ? Visibility.Visible : Visibility.Hidden;
+		}
+
+		public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	};
+}
