d8ad3e217853f49f41ab1b2a7372af646b03477e
Josef Lintz <yosi1.lints@gmail.com>
2019-08-12 23:48:56 +0200

    + Added basic static DI functionality

--- XPlace-ProjectNotifier/DI/DI.cs
+++ XPlace-ProjectNotifier/DI/DI.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace XPlace_ProjectNotifier
+﻿namespace XPlace_ProjectNotifier
 {
-	public class DI
+	using System;
+	using Microsoft.Extensions.DependencyInjection;
+
+	/// <summary>
+	/// Main DI class for this application
+	/// </summary>
+	public static class DI
 	{
+		public static ServiceProvider Provider;
+
+		/// <summary>
+		/// Returns a service from the <see cref="Provider"/>
+		/// </summary>
+		/// <typeparam name="TService"> The type of service </typeparam>
+		/// <returns></returns>
+		public static TService GetService<TService>()
+		{
+			return Provider.GetService<TService>();
+		}
+
+
+		/// <summary>
+		/// Sets up the injecting service provider 
+		/// </summary>
+		/// <param name="provider"></param>
+		public static void SetupDI(ServiceProvider provider)
+		{
+			Provider = provider;
+		}
 	}
 }
