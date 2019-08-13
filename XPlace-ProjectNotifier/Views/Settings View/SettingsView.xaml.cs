5b639d10a64baed729bd362299cc403bc30892c6
Josef Lintz <yosi1.lints@gmail.com>
2019-08-13 17:45:29 +0200

    + Added basic View for settings page

---
+++ XPlace-ProjectNotifier/Views/Settings View/SettingsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for SettingsView.xaml
+	/// </summary>
+	public partial class SettingsView : UserControl
+	{
+		public SettingsView()
+		{
+			InitializeComponent();
+		}
+	};
+};
