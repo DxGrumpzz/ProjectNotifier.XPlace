8d3739870aa793a55fea54f3ba323c8676aafa17
Josef Lintz <yosi1.lints@gmail.com>
2019-08-13 18:24:32 +0200

    + Added an attached property that sets a textbox to accept only numeric characters (0-9)

---
+++ XPlace-ProjectNotifier/Attached Properties/TextAttachedProperties.cs
@@ -0,0 +1,49 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+	using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Text;
+	using System.Windows;
+	using System.Windows.Controls;
+
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public class IsOnlyNumericTextBlock
+	{
+		private const string NUMERIC_REGEX_PATTERN = "[0-9]";
+
+		public static bool GetValue(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(MyPropertyProperty);
+		}
+
+		public static void SetValue(DependencyObject obj, bool value)
+		{
+			obj.SetValue(MyPropertyProperty, value);
+		}
+
+
+		public static readonly DependencyProperty MyPropertyProperty =
+			DependencyProperty.RegisterAttached("Value",
+				typeof(bool),
+				typeof(IsOnlyNumericTextBlock),
+				new PropertyMetadata(false, new PropertyChangedCallback(ValueCallback)));
+
+		private static void ValueCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			// Validation
+			if(!(d is TextBox textBox))
+				return;
+
+			textBox.TextChanged += (sender, _e) =>
+			{
+				textBox.IsTextValid(NUMERIC_REGEX_PATTERN);
+			};
+		}
+
+
+	};
+}
