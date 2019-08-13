74607064762ac9930e0f9fba86d1a3bdb2179064
Josef Lintz <yosi1.lints@gmail.com>
2019-08-13 12:37:45 +0200

    + Added a settings model class

---
+++ XPlace-ProjectNotifier/Models/SettingsModel.cs
@@ -0,0 +1,21 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+
+	/// <summary>
+	/// A model class that contains the app's settings
+	/// </summary>
+	public class SettingsModel
+	{
+
+		/// <summary>
+		/// How many projects to read/display
+		/// </summary>
+		public int ProjectsCount { get; set; }
+
+
+	};
+}
