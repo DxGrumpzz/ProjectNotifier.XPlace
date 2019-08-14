af8082d05a7b84890fc81355a1c2c5853325fe14
Josef Lintz <yosi1.lints@gmail.com>
2019-08-14 12:11:35 +0200

    * Renamed NumericTextEntry to TextEntryControl, And added basic code

---
+++ XPlace-ProjectNotifier/Views/Settings View/TextEntryControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for NumericTextEntry.xaml
+	/// </summary>
+	public partial class TextEntryControl : UserControl
+	{
+		public TextEntryControl()
+		{
+			InitializeComponent();
+		}
+	};
+};
