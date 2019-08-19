3a31c441350780f73d9b81a2c9dcf161cb72ca33
Josef Lintz <yosi1.lints@gmail.com>
2019-08-19 13:36:15 +0200

    + Added a basic implementation of a logger

---
+++ XPlace-ProjectNotifier/Logger/ILoggerBase.cs
@@ -0,0 +1,47 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// A basic implementation of a logger
+    /// </summary>
+    public interface ILoggerBase
+	{
+
+		#region Events
+
+		/// <summary>
+		/// An action that will be invoked when a new log is logged 
+		/// </summary>
+		public event Action<string, LogLevel> NewLog;
+
+		#endregion
+
+
+		#region Properties
+
+		/// <summary>
+		/// The logger's verboseness level 
+		/// </summary>
+		public LogLevel LogOutputLevel { get; set; }
+
+		#endregion
+
+
+		#region Methods
+
+		/// <summary>
+		/// Logs a message
+		/// </summary>
+		/// <param name="logMessage"> The message to log </param>
+		/// <param name="logLevel"> The log's output severity </param>
+		/// <param name="callerOrigin"> Where the logger was called from </param>
+		/// <param name="filePath"> The path where the log came from </param>
+		/// <param name="lineNumber"> The line number where the log was called </param>
+		public void Log(string logMessage, LogLevel logLevel = LogLevel.Normal, [CallerMemberName]string callerOrigin = "", string filePath = "", int lineNumber = 0);
+
+		#endregion
+
+	};
+};
