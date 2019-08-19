e9d6f98488e6c7743a9d1abad576915483e2be0f
Josef Lintz <yosi1.lints@gmail.com>
2019-08-19 12:51:15 +0200

    + Added a file localization class

---
+++ XPlace-ProjectNotifier/Localization/AppFiles.cs
@@ -0,0 +1,16 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+	/// <summary>
+	/// A static class that contains Files and paths to files that are used/required by this app
+	/// </summary>
+	public static class AppFiles
+	{
+
+
+
+	};
+};
