ca90ec2694fdc8fdcf3f9d5f02526319782b6c38
Josef Lintz <yosi1.lints@gmail.com>
2019-08-19 12:57:30 +0200

    + Added some soon-to-be important files to AppFiles class

--- XPlace-ProjectNotifier/Localization/AppFiles.cs
+++ XPlace-ProjectNotifier/Localization/AppFiles.cs
@@ -9,8 +9,30 @@
 	/// </summary>
 	public static class AppFiles
 	{
+		#region Constants
+
+		/// <summary>
+		/// The name of the main config file
+		/// </summary>
+		private const string CONFIG_FILE_NAME = "Config.json";
+
+		/// <summary>
+		/// The name of the log file
+		/// </summary>
+		private const string LOG_FILE_NAME = "Log.txt";
+
+		#endregion
 
 
+		/// <summary>
+		/// The name of the main config file
+		/// </summary>
+		public static string ConfigFileName => CONFIG_FILE_NAME;
+
+		/// <summary>
+		/// The name of the log file
+		/// </summary>
+		public static string LogFileName => LOG_FILE_NAME;
 
 	};
 };
