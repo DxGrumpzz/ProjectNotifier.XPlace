91f860efe03e539c90e6ed5ae343fcb668b8f2c8
Josef Lintz <yosi1.lints@gmail.com>
2019-08-20 17:27:46 +0200

    + Added: When user saved project count setting the project list will be reloaded with the new project count

--- XPlace-ProjectNotifier/Viewmodels/MainWindowViewModel.cs
+++ XPlace-ProjectNotifier/Viewmodels/MainWindowViewModel.cs
@@ -3,10 +3,8 @@
 	using System;
 	using System.Linq;
 	using System.Collections.ObjectModel;
-	using System.Xml;
 	using System.Diagnostics;
 	using System.Threading.Tasks;
-	using System.Xml.Linq;
 
 	public class MainWindowViewModel : BaseViewModel
 	{
@@ -52,15 +50,6 @@
 
 		public SettingsViewModel SettingsViewModel { get; }
 
-		#endregion
-
-
-		#region Commands
-
-		public RelayCommand OpenSettingsCommand { get; }
-
-		#endregion
-
 		public bool IsLoading
 		{
 			get => _isLoading;
@@ -71,18 +60,25 @@
 			}
 		}
 
+		#endregion
+
+
+		#region Commands
+
+		public RelayCommand OpenSettingsCommand { get; }
+
+		#endregion
+
 
 
 		public MainWindowViewModel() { }
 
-		public MainWindowViewModel(SettingsModel settingsModel) :
-			this()
+		public MainWindowViewModel(SettingsModel settingsModel)
 		{
 			SettingsModel = settingsModel;
-			SettingsViewModel = new SettingsViewModel(settingsModel)
-			{
-			};
+			SettingsViewModel = new SettingsViewModel(settingsModel);
 
+			
 
 			Task.Run(SetupRSSProjectListAsync);
 
@@ -90,6 +86,20 @@
 			{
 				SettingsViewModel.IsOpen = true;
 			});
+
+
+			// When project count setting is saved...
+			SettingsViewModel.ProjectCountSetting.SaveChangesAction += async (value) =>
+			{
+				// Reset project list
+				ProjectList.ProjectList = new ObservableCollection<ProjectItemViewModel>();
+
+				// Display loading text
+				IsLoading = true;
+
+				// Load new project list
+				await SetupRSSProjectListAsync();
+			};
 		}
 
 
@@ -104,10 +114,9 @@
 			await Task.Run(() =>
 			{
 				// Read rss feed
-				RSSReader rssReader = new RSSReader("https://www.xplace.com/il/rss/new-projects");
-
+				var projects = new RSSReader("https://www.xplace.com/il/rss/new-projects")
 				// Grab however many results the user requested from the RSS feed
-				var projects = rssReader.GetXElementNodeList(count: SettingsModel.ProjectsToDisplay)
+				.GetXElementNodeList(count: SettingsModel.ProjectsToDisplay)
 				// "Convert" the xml data to a ProjectModel
 				.Select(element =>
 				{
