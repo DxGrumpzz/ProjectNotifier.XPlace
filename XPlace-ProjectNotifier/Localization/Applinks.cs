13f71fe1b4660becf5d1e671bca6eb223aa836e1
Josef Lintz <yosi1.lints@gmail.com>
2019-08-20 18:12:33 +0200

    + Added app links to localization

---
+++ XPlace-ProjectNotifier/Localization/Applinks.cs
@@ -0,0 +1,19 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+	/// <summary>
+	/// url's and links that are used by this app
+	/// </summary>
+	public static class AppLinks
+	{
+		
+		/// <summary>
+		/// Link to the RSS feed that is consumed by this app
+		/// </summary>
+		public static string RSSFeedUrl => "https://www.xplace.com/il/rss/new-projects";
+
+	};
+};
