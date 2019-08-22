f80b16f03c876b49894282db31a338b0b793c35e
Josef Lintz <yosi1.lints@gmail.com>
2019-08-22 12:02:48 +0200

    + Added ProjkectNotification a window that will be used as a Toast notification to show the user that there is a new project available

---
+++ XPlace-ProjectNotifier/Views/ProjectNotification View/ProjectNotificationView.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace XPlace_ProjectNotifier.Views.ProjectNotification_View
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+	using System.Windows;
+	using System.Windows.Controls;
+	using System.Windows.Data;
+	using System.Windows.Documents;
+	using System.Windows.Input;
+	using System.Windows.Media;
+	using System.Windows.Media.Imaging;
+	using System.Windows.Shapes;
+
+	/// <summary>
+	/// Interaction logic for ProjectNotificationView.xaml
+	/// </summary>
+	public partial class ProjectNotificationView : Window
+	{
+		public ProjectNotificationView()
+		{
+			InitializeComponent();
+		}
+	};
+};
