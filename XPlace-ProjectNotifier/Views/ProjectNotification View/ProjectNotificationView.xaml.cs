2a8af96aa32c68e89c351f188b885ee12eb9f20e
Josef Lintz <yosi1.lints@gmail.com>
2019-08-25 13:11:14 +0200

    + Added constructor that takes a ProjectNotication viewmodel to ProjectNotificationView

--- XPlace-ProjectNotifier/Views/ProjectNotification View/ProjectNotificationView.xaml.cs
+++ XPlace-ProjectNotifier/Views/ProjectNotification View/ProjectNotificationView.xaml.cs
@@ -1,24 +1,26 @@
-﻿namespace XPlace_ProjectNotifier.Views.ProjectNotification_View
+﻿namespace XPlace_ProjectNotifier
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Text;
 	using System.Windows;
-	using System.Windows.Controls;
-	using System.Windows.Data;
-	using System.Windows.Documents;
-	using System.Windows.Input;
-	using System.Windows.Media;
-	using System.Windows.Media.Imaging;
-	using System.Windows.Shapes;
 
 	/// <summary>
 	/// Interaction logic for ProjectNotificationView.xaml
 	/// </summary>
 	public partial class ProjectNotificationView : Window
 	{
+
 		public ProjectNotificationView()
 		{
+			DataContext = new ProjectNotificationViewModel();
+			((ProjectNotificationViewModel)DataContext).BindWindow(this);
+
+			InitializeComponent();
+		}
+
+		public ProjectNotificationView(ProjectNotificationViewModel viewModel)
+		{
+			DataContext = viewModel;
+			viewModel.BindWindow(this);
+
 			InitializeComponent();
 		}
 	};
