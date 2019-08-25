a24231b6cc030d004c9b53d596cbe2c6e9b7402c
Josef Lintz <yosi1.lints@gmail.com>
2019-08-25 18:05:00 +0200

    + Added dispatcher call, function to UIManager. Queues an action to be executed on the main thread

--- XPlace-ProjectNotifier/UI Manager/Base/IUIManager.cs
+++ XPlace-ProjectNotifier/UI Manager/Base/IUIManager.cs
@@ -9,6 +9,12 @@
 	/// </summary>
 	public interface IUIManager
 	{
+		/// <summary>
+		/// Returns execution back to the main thread. To be used only for UI components
+		/// </summary>
+		/// <param name="action"> The function or code to execute </param>
+		public void Dispatch(Action action);
+
 
 		/// <summary>
 		/// Create a <see cref="ProjectNotificationView "/> notification and display it
--- XPlace-ProjectNotifier/UI Manager/UIManager.cs
+++ XPlace-ProjectNotifier/UI Manager/UIManager.cs
@@ -1,8 +1,7 @@
 ﻿namespace XPlace_ProjectNotifier
 {
 	using System;
-	using System.Collections.Generic;
-	using System.Text;
+	using System.Windows;
 
 	/// <summary>
 	/// The main UI manager for this app
@@ -10,16 +9,29 @@
 	public class UIManager : IUIManager
 	{
 		/// <summary>
+		/// Returns execution back to the main thread. To be used only for UI components
+		/// </summary>
+		/// <param name="action"> The function or code to execute </param>
+		public void Dispatch(Action action)
+		{
+			// Invoke dispatcher and execute code
+			Application.Current.Dispatcher.Invoke(action);
+		}
+
+		/// <summary>
 		/// Create a <see cref="ProjectNotificationView "/> notification and display it 
 		/// </summary>
 		/// <param name="projectNotificationViewModel"></param>
 		/// <returns></returns>
 		public void ShowProjectNotification(ProjectNotificationViewModel projectNotificationViewModel)
 		{
-			// Create instance of Project notification
-			new ProjectNotificationView(projectNotificationViewModel)
-			// show notification
-			.Show();
+			Dispatch(() =>
+			{
+				// Create instance of Project notification
+				new ProjectNotificationView(projectNotificationViewModel)
+				// show notification
+				.Show();
+			});
 		}
 	};
 };
