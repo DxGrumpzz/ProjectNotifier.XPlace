d9cccd753d6e5e7f9a998de837a3d319dbd94607
Josef Lintz <yosi1.lints@gmail.com>
2019-08-26 14:16:49 +0200

    + Added a view model for the project notification item

---
+++ XPlace-ProjectNotifier/Viewmodels/ProjectNotification ViewModel/ProjectNotificationItemViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System;
+	using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Text;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ProjectNotificationItemViewModel
+	{
+
+		public ProjectModel ProjectModel { get; set; }
+
+
+		public RelayCommand OpenProjectCommand { get; }
+
+
+		public ProjectNotificationItemViewModel()
+		{
+			OpenProjectCommand = new RelayCommand(ExecuteOpenProjectCommand);
+		}
+
+
+		private void ExecuteOpenProjectCommand()
+		{
+			// Open project link in Default browser
+			// (thank you .net core 3 for needing to open a cmd to launch a url)
+			Process.Start(new ProcessStartInfo
+			{
+				FileName = "cmd",
+				Arguments = $"/c start {ProjectModel.Link}",
+				// Ensures that the user doesn't see the cmd window opening
+				WindowStyle = ProcessWindowStyle.Hidden,
+				UseShellExecute = false,
+				CreateNoWindow = true,
+			});
+		}
+	};
+};
