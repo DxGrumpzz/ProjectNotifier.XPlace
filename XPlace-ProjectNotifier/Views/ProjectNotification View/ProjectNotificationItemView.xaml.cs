85040bfe85347c541a4eaf0ef64596dcc98b66c0
Josef Lintz <yosi1.lints@gmail.com>
2019-08-26 14:13:59 +0200

    + Added ProjectNotificationItemView a "slimmer" version of a project item

---
+++ XPlace-ProjectNotifier/Views/ProjectNotification View/ProjectNotificationItemView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace XPlace_ProjectNotifier
+{
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for ProjectNotificationItemView.xaml
+	/// </summary>
+	public partial class ProjectNotificationItemView : UserControl
+	{
+		public ProjectNotificationItemView()
+		{
+			InitializeComponent();
+		}
+	};
+};
