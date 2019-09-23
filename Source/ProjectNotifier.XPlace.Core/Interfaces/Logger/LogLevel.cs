153af8b80720e0337c6be3d09cfa2079c4a04254
Josef Lintz <yosi1.lints@gmail.com>
2019-09-23 13:30:57 +0300

    + Added Logger stuff to core

---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/Logger/LogLevel.cs
@@ -0,0 +1,33 @@
+namespace ProjectNotifier.XPlace.Core
+{
+	/// <summary>
+	/// The level/severity of a logged message
+	/// </summary>
+	public enum LogLevel
+	{
+		/// <summary>
+		/// Log all information
+		/// </summary>
+		Debug = 0,
+
+		/// <summary>
+		/// Log most information
+		/// </summary>
+		Verbose = 1,
+
+		/// <summary>
+		/// Log all informative information
+		/// </summary>
+		Informative = 2,
+
+		/// <summary>
+		/// Logs wanring, errors, and standard messages
+		/// </summary>
+		Normal = 3,
+
+		/// <summary>
+		/// Log only critical errors and warnings
+		/// </summary>
+		Critical = 4,
+	};
+};
