71abf4255dfff3b93856dd0ec75472f6c1c8a197
Josef Lintz <yosi1.lints@gmail.com>
2019-09-23 13:38:19 +0300

    + Added Models folder and moved project model to core dll

---
+++ Source/ProjectNotifier.XPlace.Core/Models/ProjectModel.cs
@@ -0,0 +1,35 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+	using System;
+
+	/// <summary>
+	/// A class that represents a Project from XPlace
+	/// </summary>
+	public class ProjectModel
+	{
+		/// <summary>
+		/// The proejct's title
+		/// </summary>
+		public string Title { get; set; }
+	
+		/// <summary>
+		/// The proejct's URL
+		/// </summary>
+		public string Link { get; set; }
+	
+		/// <summary>
+		/// The proejct's description
+		/// </summary>
+		public string Description { get; set; }
+	
+		/// <summary>
+		/// The proejct's publishing date
+		/// </summary>
+		public DateTime PublishingDate { get; set; }
+
+		/// <summary>
+		/// A unique ID number for this project
+		/// </summary>
+		public int ProjectID { get; set; }
+	};
+}
