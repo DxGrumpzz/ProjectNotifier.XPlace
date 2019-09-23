7dd73b09ffd9f6623c8034f4f5cd57486beec1e0
Josef Lintz <yosi1.lints@gmail.com>
2019-09-23 16:44:35 +0300

    * Added IDI (IDependencyInjection) a container for the app(s) dependency injection provider

---
+++ Source/ProjectNotifier.XPlace.Core/DI/IDI.cs
@@ -0,0 +1,33 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+	using Microsoft.Extensions.DependencyInjection;
+
+
+	/// <summary>
+	/// Specifies the implementation of a Dependency injection provider
+	/// </summary>
+	public interface IDI
+	{
+
+		/// <summary>
+		/// Main provider
+		/// </summary>
+		public ServiceProvider Provider { get; }
+
+
+		/// <summary>
+		/// Returns a service from the <see cref="Provider"/>
+		/// </summary>
+		/// <typeparam name="TService"> The type of service </typeparam>
+		/// <returns></returns>
+		public TService GetService<TService>();
+
+
+		/// <summary>
+		/// Sets up the injecting service provider 
+		/// </summary>
+		/// <param name="provider"></param>
+		public void SetupDI(ServiceProvider provider);
+
+	};
+}
