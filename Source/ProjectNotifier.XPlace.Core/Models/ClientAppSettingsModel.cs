74542d5d68be4338c121920e613687ab6412a229
Josef Lintz <yosi1.lints@gmail.com>
2019-09-23 13:41:40 +0300

    * Renamed SettingsModel to ClientAppSettingsModel, This class/model will be used only by the client app

---
+++ Source/ProjectNotifier.XPlace.Core/Models/ClientAppSettingsModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+	/// <summary>
+	/// A model class that contains the app's settings
+	/// </summary>
+	public class ClientAppSettingsModel
+	{
+
+		/// <summary>
+		/// How many projects to read/display
+		/// </summary>
+		public int ProjectsToDisplay { get; set; }
+
+		/// <summary>
+		/// How long to display the new projects notification in seconds
+		/// </summary>
+		public int KeepNotificationOpenSeconds { get; set; }
+
+	};
+}
