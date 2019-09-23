c5b7fd6cc620679451c97d0c58d84f94c67278a7
Josef Lintz <yosi1.lints@gmail.com>
2019-09-23 13:37:55 +0300

    + Added ui manager to Core dll

---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/UI Manager/IUIManager.cs
@@ -0,0 +1,26 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+	using System;
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// An interface for a UI manager. Responsible for showing notifications and such
+	/// </summary>
+	public interface IUIManager
+	{
+		/// <summary>
+		/// Returns execution back to the main thread. To be used only for UI components
+		/// </summary>
+		/// <param name="action"> The function or code to execute </param>
+		public void Dispatch(Action action);
+
+
+		/// <summary>
+		/// Create a <see cref="ProjectNotificationView "/> notification and display it
+		/// </summary>
+		/// <param name="newProjects"></param>
+		/// <returns></returns>
+		public void ShowProjectNotification(IEnumerable<ProjectModel> newProjects);
+
+	};
+}
