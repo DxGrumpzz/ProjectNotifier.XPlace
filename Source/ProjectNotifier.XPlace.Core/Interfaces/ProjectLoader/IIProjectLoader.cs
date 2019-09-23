7410a144602aad4a26cd4e79bca3a4d84638a235
Josef Lintz <yosi1.lints@gmail.com>
2019-09-23 17:07:12 +0300

    + Added an interface that specifies an implementation of a project loader

---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/ProjectLoader/IIProjectLoader.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+
+	using ProjectNotifier.XPlace.Core;
+	using System;
+	using System.Collections.Generic;
+
+
+	/// <summary>
+	/// Specifies an implementation of a project loader/reader
+	/// </summary>
+	public interface IIProjectLoader
+	{
+
+		/// <summary>
+		/// An event that will be invkoed when the ProjectLoader has loaded a new list of projects
+		/// </summary>
+		public event Action<ProjectModel> ProjectsListUpdated;
+
+
+		/// <summary>
+		/// Returns a <see cref="ProjectListViewModel"/> containing a list of projects
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<ProjectModel> LoadProjects();
+
+	}
+}
