d58fcc3d1f0ab712bfebc77f7dc657323652fcdd
Josef Lintz <yosi1.lints@gmail.com>
2019-09-24 15:35:29 +0300

    + Added register and login page viewmodels

---
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Login ViewModel/LoginViewModel.cs
@@ -0,0 +1,53 @@
+﻿using ProjectNotifier.XPlace.Core;
+
+namespace ProjectNotifier.XPlace.Client
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LoginViewModel : BaseViewModel
+    {
+
+        public static LoginViewModel Instance => new LoginViewModel()
+        {
+
+        };
+
+        private string _username;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #region Commands
+
+        public RelayCommand GotoRegisterPageCommand { get; }
+
+        #endregion
+
+
+        public LoginViewModel()
+        {
+            GotoRegisterPageCommand = new RelayCommand(ExecuteGotoRegisterPageCommand);
+        }
+
+
+        #region Command callbacks
+
+        private void ExecuteGotoRegisterPageCommand()
+        {
+            DI.GetService<MainWindowViewModel>().CurrentPage = new RegisterView(new RegisterViewModel());
+
+        }
+
+        #endregion
+
+    };
+};
---
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Register Viewmodel/RegisterViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using ProjectNotifier.XPlace.Core;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RegisterViewModel : BaseViewModel
+    {
+
+        public static RegisterViewModel Intance => new RegisterViewModel()
+        {
+
+        };
+
+
+        #region Commands
+
+        public RelayCommand GotoLoginPageCommand { get; }
+
+        #endregion
+
+
+        public RegisterViewModel()
+        {
+            GotoLoginPageCommand = new RelayCommand(ExecuteGotoLoginPageCommand);
+        }
+
+        #region Command callbacks
+
+
+
+        private void ExecuteGotoLoginPageCommand()
+        {
+            DI.GetService<MainWindowViewModel>().CurrentPage = new LoginView(new LoginViewModel());
+        }
+
+        #endregion
+
+    };
+};
