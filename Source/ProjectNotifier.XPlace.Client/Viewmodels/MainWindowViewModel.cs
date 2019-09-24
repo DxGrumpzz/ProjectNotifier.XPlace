2cb12aa54976bdd83cae172f7a4f1076ff0189a6
Josef Lintz <yosi1.lints@gmail.com>
2019-09-24 15:36:12 +0300

    * Switched positions of login and register buttons in Registration view

--- Source/ProjectNotifier.XPlace.Client/Viewmodels/MainWindowViewModel.cs
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/MainWindowViewModel.cs
@@ -1,167 +1,187 @@
 ﻿namespace ProjectNotifier.XPlace.Client
 {
-	using System;
-	using System.Linq;
-	using System.Collections.ObjectModel;
-	using System.Diagnostics;
-	using System.Threading.Tasks;
-	using System.Collections.Generic;
-	using ProjectNotifier.XPlace.Core;
+    using ProjectNotifier.XPlace.Core;
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
 
-	public class MainWindowViewModel : BaseViewModel
-	{
+    public class MainWindowViewModel : BaseViewModel
+    {
 
-		#region Private fields
+        #region Private fields
 
-		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
-		private MainWindowModel _model;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private MainWindowModel _model;
 
-		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
-		private bool _isLoading = true;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private bool _isLoading = true;
 
-		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
-		private ProjectListViewModel projectListViewModel;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private ProjectListViewModel projectListViewModel;
 
-		#endregion
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private object _currentPage;
 
-
-		#region Public properties
-
-		public MainWindowModel Model
-		{
-			get => _model;
-			set
-			{
-				_model = value;
-				OnPropertyChanged();
-			}
-		}
-
-		public ProjectListViewModel ProjectList
-		{
-			get => projectListViewModel;
-			set
-			{
-				projectListViewModel = value;
-				OnPropertyChanged();
-
-			}
-		}
-
-		public ClientAppSettingsModel ClientAppSettingsModel { get; }
-
-		public SettingsViewModel SettingsViewModel { get; }
-
-		public ProjectLoader ProjectLoader { get; }
+        #endregion
 
 
-		public bool IsLoading
-		{
-			get => _isLoading;
-			private set
-			{
-				_isLoading = value;
-				OnPropertyChanged();
-			}
-		}
+        #region Public properties
 
-		#endregion
+        public MainWindowModel Model
+        {
+            get => _model;
+            set
+            {
+                _model = value;
+                OnPropertyChanged();
+            }
+        }
 
+        public ProjectListViewModel ProjectList
+        {
+            get => projectListViewModel;
+            set
+            {
+                projectListViewModel = value;
+                OnPropertyChanged();
 
-		#region Commands
+            }
+        }
 
-		public RelayCommand OpenSettingsCommand { get; }
+        public ClientAppSettingsModel ClientAppSettingsModel { get; }
 
-		public RelayCommand Command { get; }
+        public SettingsViewModel SettingsViewModel { get; }
 
-		#endregion
+        public ProjectLoader ProjectLoader { get; }
 
 
 
-		public MainWindowViewModel() { }
-
-		public MainWindowViewModel(ClientAppSettingsModel clientAppSettingsModel, ProjectLoader projectLoader)
-		{
-			ClientAppSettingsModel = clientAppSettingsModel;
-			SettingsViewModel = new SettingsViewModel(clientAppSettingsModel);
-			ProjectLoader = projectLoader;
-
-
-			Task.Run(SetupRSSProjectListAsync);
-			ProjectLoader.StartAutoUpdating();
+        public object CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged();
+            }
+        }
 
 
-			OpenSettingsCommand = new RelayCommand(() =>
-			{
-				SettingsViewModel.IsOpen = true;
-			});
+        public bool IsLoading
+        {
+            get => _isLoading;
+            private set
+            {
+                _isLoading = value;
+                OnPropertyChanged();
+            }
+        }
 
-			// When project count setting is saved...
-			SettingsViewModel.ProjectCountSetting.SaveChangesAction += new Action<TextEntryViewModel<int>>(async (value) =>
-			{
-				// Reset project list
-				ProjectList.ProjectList = new ObservableCollection<ProjectItemViewModel>();
-
-				// Display loading text
-				IsLoading = true;
-
-				// Load new project list
-				await SetupRSSProjectListAsync();
-			});
+        #endregion
 
 
-			ProjectLoader.ProjectsListUpdated += (newProjectList) =>
-			{
-				var currentProjectListFirstProject = ProjectList.ProjectList.First().ProjectModel;
-				var newProjectListFirstProject = newProjectList.ProjectList.First().ProjectModel;
+        #region Commands
 
-				// Comapre the 2 projects 
-				if(currentProjectListFirstProject.ProjectID != newProjectListFirstProject.ProjectID)
-				{
-					// Find the newer projects
-					var newProjectsList = newProjectList.ProjectList
-					.Where(model =>
-					{
-						return model.ProjectModel.ProjectID > currentProjectListFirstProject.ProjectID;
-					})
-					// Convert results into ProjectModel
-					.Select(result => new ProjectModel()
-					{
-						Title = result.ProjectModel.Title,
-						Link = result.ProjectModel.Link,
-					});
+        public RelayCommand OpenSettingsCommand { get; }
+
+        public RelayCommand Command { get; }
+
+        #endregion
 
 
-					// update list
-					ProjectList = new ProjectListViewModel();
-					IsLoading = true;
-					ProjectList = newProjectList;
-					IsLoading = false;
+
+        public MainWindowViewModel() { }
+
+        public MainWindowViewModel(ClientAppSettingsModel clientAppSettingsModel, ProjectLoader projectLoader)
+        {
+            ClientAppSettingsModel = clientAppSettingsModel;
+            SettingsViewModel = new SettingsViewModel(clientAppSettingsModel);
+            ProjectLoader = projectLoader;
 
 
-					// Notify user for the new projects
-					DI.UIManager().ShowProjectNotification(newProjectsList);
-				};
-			};
-		}
+            CurrentPage = new LoginView(new LoginViewModel()
+            {
+                Username = "asadfasdf",
+            });
 
 
-		#region Private methods
+            Task.Run(SetupRSSProjectListAsync);
+            ProjectLoader.StartAutoUpdating();
 
-		/// <summary>
-		/// Asynchrounsly loads rss feed content
-		/// </summary>
-		/// <returns></returns>
-		private async Task SetupRSSProjectListAsync()
-		{
-			// Load projects
-			await Task.Run(() => ProjectList = ProjectLoader.LoadProjects());
 
-			// Finished loading content
-			IsLoading = false;
-		}
+            OpenSettingsCommand = new RelayCommand(() =>
+            {
+                SettingsViewModel.IsOpen = true;
+            });
 
-		#endregion
+            // When project count setting is saved...
+            SettingsViewModel.ProjectCountSetting.SaveChangesAction += new Action<TextEntryViewModel<int>>(async (value) =>
+            {
+                // Reset project list
+                ProjectList.ProjectList = new ObservableCollection<ProjectItemViewModel>();
 
-	}
+                // Display loading text
+                IsLoading = true;
+
+                // Load new project list
+                await SetupRSSProjectListAsync();
+            });
+
+
+            ProjectLoader.ProjectsListUpdated += (newProjectList) =>
+            {
+                var currentProjectListFirstProject = ProjectList.ProjectList.First().ProjectModel;
+                var newProjectListFirstProject = newProjectList.ProjectList.First().ProjectModel;
+
+                // Comapre the 2 projects 
+                if(currentProjectListFirstProject.ProjectID != newProjectListFirstProject.ProjectID)
+                {
+                    // Find the newer projects
+                    var newProjectsList = newProjectList.ProjectList
+                    .Where(model =>
+                    {
+                        return model.ProjectModel.ProjectID > currentProjectListFirstProject.ProjectID;
+                    })
+                    // Convert results into ProjectModel
+                    .Select(result => new ProjectModel()
+                    {
+                        Title = result.ProjectModel.Title,
+                        Link = result.ProjectModel.Link,
+                    });
+
+
+                    // update list
+                    ProjectList = new ProjectListViewModel();
+                    IsLoading = true;
+                    ProjectList = newProjectList;
+                    IsLoading = false;
+
+
+                    // Notify user for the new projects
+                    DI.UIManager().ShowProjectNotification(newProjectsList);
+                };
+            };
+        }
+
+
+        #region Private methods
+
+        /// <summary>
+        /// Asynchrounsly loads rss feed content
+        /// </summary>
+        /// <returns></returns>
+        private async Task SetupRSSProjectListAsync()
+        {
+            // Load projects
+            await Task.Run(() => ProjectList = ProjectLoader.LoadProjects());
+
+            // Finished loading content
+            IsLoading = false;
+        }
+
+        #endregion
+
+    }
 }
---
+++ Source/ProjectNotifier.XPlace.Core/AutoNotifier/AutoNotifier.cs
@@ -0,0 +1,96 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+	using System;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// A class that invokes an action every so often
+	/// </summary>
+	public class AutoNotifier
+	{
+
+		#region Public properties
+
+		/// <summary>
+		/// Interval in milliseconds that indicates how often to invoke notification
+		/// </summary>
+		public int Interval { get; }
+
+		/// <summary>
+		/// A boolean flag that indicates if currently auto notifing
+		/// </summary>
+		public bool NotifierWorking { get; private set; }
+
+		#endregion
+
+
+		#region Events
+
+		/// <summary>
+		/// The event/notification that will be invoked
+		/// </summary>
+		public event Action Notification = () => { };
+
+
+		#endregion
+
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		/// <param name="intervalInMS"> A set period where an event will be fired </param>
+		public AutoNotifier(int intervalInMS = 5000)
+		{
+			Interval = intervalInMS;
+		}
+
+
+		/// <summary>
+		/// Starts a background thread that will invoke <see cref="Notification"/> every <see cref="Interval"/>
+		/// </summary>
+		public void StartAutoNotifing(CancellationToken cancellationToken = default)
+		{
+			// Don't run if already updating
+			if(NotifierWorking == true)
+				return;
+
+			// If notification is null (Because it was cancelled)
+			if(Notification is null)
+				// Re-Instantiate
+				Notification = () => { };
+			
+
+			// Set updating flag to true
+			NotifierWorking = true;
+
+
+			// Run a background task
+			Task.Run(async () =>
+			{
+				// While updating flag is true
+				while(NotifierWorking == true)
+				{
+					// Wait for interval
+					await Task.Delay(Interval);
+
+					// Invoke event
+					Notification?.Invoke();
+				};
+			}, cancellationToken);
+		}
+
+
+		/// <summary>
+		/// Stops auto notifing
+		/// </summary>
+		public void StopAutoNotifing()
+		{
+			// Disable notification event
+			Notification = null;
+
+			// Set auto updating flag to false
+			NotifierWorking = false;
+		}
+	};
+};
