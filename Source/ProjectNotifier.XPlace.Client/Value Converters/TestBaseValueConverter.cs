3c92b3337fbb394d3030ae9e2ef06ded399776e5
Josef Lintz <yosi1.lints@gmail.com>
2019-09-24 19:03:58 +0300

    * Testing a Generic ValueConverter

---
+++ Source/ProjectNotifier.XPlace.Client/Value Converters/TestBaseValueConverter.cs
@@ -0,0 +1,55 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Markup;
+
+    /// <summary>
+    /// A base ValueConverter for all <see cref="IValueConverter"/> classes to inherit.
+    /// Allows for a more simple implmentation in Xaml
+    /// </summary>
+    /// <typeparam name="T"> The type of class or ValueConverter </typeparam>
+    public abstract class TestBaseValueConverter<T, TValue, TParam> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+
+        /// <summary>
+        /// A single static instance of this value converter
+        /// </summary>
+        private static T Converter = null;
+
+
+        /// <summary>
+        /// Converts a value into another value with a possibility to pass a specifc value with the parameter
+        /// </summary>
+        /// <param name="value"> The value to convert </param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"> </param>
+        /// <param name="culture"></param>
+        /// <returns> Returns an <see cref="object"/> that can be cast into any other object </returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ConvertT((TValue)value, targetType, (TParam)parameter, culture);
+        }
+
+
+        public abstract object ConvertT(TValue value, Type targetType, TParam parameter, CultureInfo culture);
+
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+
+
+        /// <summary>
+        /// Provides a static instance of the value converter 
+        /// </summary>
+        /// <param name="serviceProvider">The service provider</param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            // If the converter instance is null create a new type of what ever type passed in the type argument
+            return Converter ?? (Converter = new T());
+        }
+
+    };
+};
