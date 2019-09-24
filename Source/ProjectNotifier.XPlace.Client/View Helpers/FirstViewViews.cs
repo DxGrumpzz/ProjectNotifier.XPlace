a6df4d72194e8d72ae1b4b2dc8d971271605382d
Josef Lintz <yosi1.lints@gmail.com>
2019-09-24 19:05:09 +0300

    + Added an enum that specifies available views for FirstView

---
+++ Source/ProjectNotifier.XPlace.Client/View Helpers/FirstViewViews.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    /// <summary>
+    /// An enumerator for the views available for the <see cref="FirstView"/>
+    /// </summary>
+    public enum FirstViewViews
+    {
+        /// <summary>
+        /// The login view
+        /// </summary>
+        Login = 0,
+
+        /// <summary>
+        /// The registration view
+        /// </summary>
+        Register = 1,
+
+    };
+};
