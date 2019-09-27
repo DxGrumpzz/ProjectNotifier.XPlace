a9e1fdaaa93e62c29ae7656cc0b00ea1bcb533db
Josef Lintz <yosi1.lints@gmail.com>
2019-09-27 14:14:27 +0300

    + Added an Enumeraator that holds the types of animations aavaailable for a view

---
+++ Source/ProjectNotifier.XPlace.Client/View Helpers/ViewAnimation.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+
+    /// <summary>
+    /// The starting animtion for when a pge loads
+    /// </summary>
+    public enum ViewAnimation
+    {
+        /// <summary>
+        /// No animation takes place
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Slides a page from the top into view
+        /// </summary>
+        SlideInFromTop = 1,
+
+        /// <summary>
+        /// Slides a page to the bottom
+        /// </summary>
+        SlideOutToBottom = 2,
+
+    }
+}
