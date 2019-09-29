252375ef4372bb83ef749563af4e89ba73ce3a04
Josef Lintz <yosi1.lints@gmail.com>
2019-09-29 15:39:58 +0300

    + Added BaseView to RegisterView

--- Source/ProjectNotifier.XPlace.Client/Views/Login and register views/Registration View/RegisterView.xaml.cs
+++ Source/ProjectNotifier.XPlace.Client/Views/Login and register views/Registration View/RegisterView.xaml.cs
@@ -5,17 +5,17 @@
     /// <summary>
     /// Interaction logic for RegisterView.xaml
     /// </summary>
-    public partial class RegisterView : UserControl
+    public partial class RegisterView : BaseView<RegisterViewModel>
     {
         public RegisterView()
         {
             InitializeComponent();
         }
 
-        public RegisterView(RegisterViewModel viewModel) :
-            this()
+        public RegisterView(RegisterViewModel viewModel) : 
+            base(viewModel)
         {
-            DataContext = viewModel;
+            InitializeComponent();
         }
     };
 };
