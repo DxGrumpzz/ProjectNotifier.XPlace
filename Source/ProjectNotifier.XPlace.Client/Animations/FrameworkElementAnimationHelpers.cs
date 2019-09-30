b64b0c23ca25d3c9a4e969fe724bcf4bcd5f0d4d
Josef Lintz <yosi1.lints@gmail.com>
2019-09-30 16:46:30 +0300

    * Fixed some typos in FrameworkElement animation helper class

--- Source/ProjectNotifier.XPlace.Client/Animations/FrameworkElementAnimationHelpers.cs
+++ Source/ProjectNotifier.XPlace.Client/Animations/FrameworkElementAnimationHelpers.cs
@@ -40,15 +40,15 @@
         /// <param name="view"> The control to animate </param>
         /// <param name="seconds"> How will the animation take place </param>
         /// <returns></returns>
-        public static async Task SlideOutToBottomAsync(this FrameworkElement view, float seconds)
+        public static async Task SlideOutToBottomAsync(this FrameworkElement control, float seconds)
         {
             Storyboard storyboard = new Storyboard();
 
             // Add slide in from top animation
-            storyboard.AddSlideIOutToBottom(seconds, view.ActualHeight);
+            storyboard.AddSlideIOutToBottom(seconds, control.ActualHeight);
 
             // Begin animation
-            storyboard.Begin(view);
+            storyboard.Begin(control);
 
             // Wait for the animation to finish
             await Task.Delay(TimeSpan.FromSeconds(seconds));
@@ -56,7 +56,7 @@
 
 
         /// <summary>
-        /// Slide a control in from the top
+        /// Slide a control out to the top
         /// </summary>
         /// <param name="control"> The control to animate </param>
         /// <param name="seconds"> How will the animation take place </param>
