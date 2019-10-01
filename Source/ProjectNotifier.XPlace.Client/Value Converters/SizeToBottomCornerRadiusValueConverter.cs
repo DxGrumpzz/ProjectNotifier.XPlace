55fc6589c292c353b20ceae891ceaa0dfde294d7
Josef Lintz <yosi1.lints@gmail.com>
2019-10-01 17:05:58 +0300

    + Added a ValueConverter that takes a width and return a bottom rounded corner radius

---
+++ Source/ProjectNotifier.XPlace.Client/Value Converters/SizeToBottomCornerRadiusValueConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Weird name, it takes the width and height of a <see cref="Border"/> and rounds it's bottom
+    /// </summary>
+    public class SizeToBottomCornerRadiusValueConverter : BaseValueConverter<SizeToBottomCornerRadiusValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new CornerRadius(0, 0, (double)value/2, (double)value/2);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    };
+};
