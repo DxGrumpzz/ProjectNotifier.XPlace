702c4788709195f5e8ea17c8981417ab892ec224
Josef Lintz <yosi1.lints@gmail.com>
2019-10-01 14:47:35 +0300

    + Added fade in and out ViewAnimations

---
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/ProjectNotification ViewModel/ProjectViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System.Collections.ObjectModel;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProjectViewModel : BaseViewModel
+    {
+
+        private ObservableCollection<ProjectItemViewModel> _projectList;
+
+        public ObservableCollection<ProjectItemViewModel> ProjectList
+        {
+            get => _projectList;
+            set
+            {
+                _projectList = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+    };
+};
---
+++ Source/ProjectNotifier.XPlace.Client/Views/Project Views/ProjectsView.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ProjectsView.xaml
+    /// </summary>
+    public partial class ProjectsView : BaseView
+    {
+        public ProjectsView()
+        {
+            ViewLoadAnimation = ViewAnimation.
+            InitializeComponent();
+        }
+    };
+};
