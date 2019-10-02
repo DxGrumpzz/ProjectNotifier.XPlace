df83c5d04de34d5447b390817b8fa96874f7eca9
Josef Lintz <yosi1.lints@gmail.com>
2019-10-02 16:34:52 +0300

    + Created a "new" project loader

--- Source/ProjectNotifier.XPlace.Client/ProjectLoader/ProjectLoader.cs
+++ Source/ProjectNotifier.XPlace.Client/ProjectLoader/ProjectLoader.cs
@@ -2,157 +2,113 @@
 {
     using ProjectNotifier.XPlace.Core;
     using System;
-	using System.Collections.ObjectModel;
-	using System.Linq;
-	using System.Threading;
-	using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Xml;
+    using System.Xml.Linq;
 
-	/// <summary>
-	/// A project loader
-	/// </summary>
-	public class ProjectLoader
-	{
+    /// <summary>
+    /// The client's implementation of a project loader
+    /// </summary>
+    public class ProjectLoader : IProjectLoader
+    {
+        
+        #region Private fields
 
-		#region Private fields
+        private readonly ClientAppSettingsModel _settings;
 
-		/// <summary>
-		/// A boolean flag that indicates if currently auto updating
+        private readonly string _url;
+
+        #endregion
+
+
+        public ProjectLoader(ClientAppSettingsModel settings, string url)
+        {
+            _settings = settings;
+            _url = url;
+        }
+
+
+        /// <summary>
+        /// Loads a list of projects from somewhere
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<ProjectModel>> LoadProjectsAsync(CancellationToken cancellationToken = default)
+        {
+            // Grab however many results the user requested from the RSS feed
+            var projects = (await GetXElementNodeListAsync(count: _settings.ProjectsToDisplay, cancellationToken: cancellationToken))
+            // "Convert" the xml data to a ProjectModel
+            .Select(element =>
+            {
+                // Select required nodes
+                var titleNode = element.Element("title").Value;
+                var linkNode = element.Element("link").Value;
+                var descriptionNode = element.Element("description").Value;
+                var publishDateNode = element.Element("pubDate").Value;
+                var projectID = Convert.ToInt32(element.Element("guid").Value);
+
+
+                return new ProjectModel()
+                {
+                    // Replace unicode identifiers(?) with string literals
+                    Title = NormalizeString(titleNode),
+                    Description = NormalizeString(descriptionNode),
+
+                    Link = linkNode,
+
+                    // Convert the date time to israel standard time
+                    PublishingDate = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.Parse(publishDateNode), "Israel Standard Time"),
+
+                    ProjectID = projectID,
+                };
+            });
+
+            return projects;
+        }
+
+
+        #region Private helpers
+
+        /// <summary>
+		/// Returns an IEnumerable of XElement
 		/// </summary>
-		private bool _isAutoUpdating = false;
-
-		/// <summary>
-		/// A cancellation token used to cancel auto update request
-		/// </summary>
-		private CancellationTokenSource _autoUpdateCancellationToken = new CancellationTokenSource();
-
-		#endregion
-
-
-		public double Interval { get; private set; }
-
-		public ClientAppSettingsModel ClientAppSettingsModel { get; }
-
-
-		/// <summary>
-		/// An event that will be invkoed when the ProjectLoader has loaded a new list of projects
-		/// </summary>
-		public event Action<ProjectListViewModel> ProjectsListUpdated = (projectList) => { };
-
-
-		public ProjectLoader(double interval, ClientAppSettingsModel clientAppSettingsModel)
-		{
-			Interval = interval;
-			ClientAppSettingsModel = clientAppSettingsModel;
-		}
-
-
-		/// <summary>
-		/// Returns a <see cref="ProjectListViewModel"/> containing a list of projects
-		/// </summary>
+		/// <param name="descendantsSelection"> The type of descendatns to select </param>
+		/// <param name="count"> How many from the top to select </param>
 		/// <returns></returns>
-		public ProjectListViewModel LoadProjects()
-		{
-			// Read rss feed
-			var projects = new RSSReader(AppLinks.RSSFeedUrl)
-			// Grab however many results the user requested from the RSS feed
-			.GetXElementNodeList(count: ClientAppSettingsModel.ProjectsToDisplay)
-			// "Convert" the xml data to a ProjectModel
-			.Select(element =>
-			{
-				// Select required nodes
-				var titleNode = element.Element("title").Value;
-				var linkNode = element.Element("link").Value;
-				var descriptionNode = element.Element("description").Value;
-				var publishDateNode = element.Element("pubDate").Value;
-				var projectID = Convert.ToInt32(element.Element("guid").Value);
+		private async Task<IEnumerable<XElement>> GetXElementNodeListAsync(string descendantsSelection = "item", int count = 25, CancellationToken cancellationToken = default)
+        {
+            using (XmlReader xmlReader = XmlReader.Create(_url, new XmlReaderSettings()
+            {
+                Async = true,
+            }))
+            {
+                // Select XML nodes from the RSS document
+                var descendatsNodes = (await XDocument.LoadAsync(xmlReader, LoadOptions.None, cancellationToken))
+                    .Descendants(descendantsSelection)
+                    .Take(count);
+
+                return descendatsNodes;
+            };
+        }
 
 
-				return new ProjectItemViewModel()
-				{
-					ProjectModel = new ProjectModel()
-					{
-						// Replace unicode identifiers(?) with string literals
-						Title = FormatString(titleNode),
-						Description = FormatString(descriptionNode),
-
-						Link = linkNode,
-
-						// Convert the date time to israel standard time
-						PublishingDate = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.Parse(publishDateNode), "Israel Standard Time"),
-
-						ProjectID = projectID,
-					},
-				};
-			});
-
-
-			return new ProjectListViewModel()
-			{
-				ProjectList = new ObservableCollection<ProjectItemViewModel>(projects),
-			};
-		}
-
-
-
-		/// <summary>
-		/// Load a list of project every Interval count, and invoke <see cref="ProjectsListUpdated"/> event
-		/// </summary>
-		public void StartAutoUpdating()
-		{
-			// Don't run if already updating
-			if(_isAutoUpdating == true)
-				return;
-
-			// Set updating flag to true
-			_isAutoUpdating = true;
-
-			// If cancellation request occured 
-			if(_autoUpdateCancellationToken.IsCancellationRequested == true)
-				// Restart cncellation token
-				_autoUpdateCancellationToken = new CancellationTokenSource();
-
-
-			// Spin a background task
-			Task.Run(async () =>
-			{
-				// While updating flag is true
-				while(_isAutoUpdating == true)
-				{
-					// Wait for interval
-					await Task.Delay((int)Interval);
-
-					// Invoke event
-					ProjectsListUpdated?.Invoke(LoadProjects());
-				};
-			}, _autoUpdateCancellationToken.Token);
-		}
-
-		/// <summary>
-		/// Stops updating checking if there is a new project(s)
-		/// </summary>
-		public void StopAutoUpdating()
-		{
-			_isAutoUpdating = false;
-			_autoUpdateCancellationToken.Cancel();
-		}
-
-
-		#region Private helpers
-
-		/// <summary>
-		/// Replaced unicode strings with string literalls
-		/// </summary>
-		/// <param name="stringToFormat"></param>
-		private string FormatString(string stringToFormat)
-		{
+        /// <summary>
+        /// Replaced unicode strings with string literalls
+        /// </summary>
+        /// <param name="stringToFormat"></param>
+        private string NormalizeString(string stringToFormat)
+        {
 #if DEBUG == TRUE
-			return stringToFormat.Replace("\n", "").Replace("&#39;", "\'").Replace("&quot;", "\"");
+            return stringToFormat.Replace("\n", "").Replace("&#39;", "\'").Replace("&quot;", "\"");
 #else
 			return stringToFormat.Replace("&#39;", "\'").Replace("&quot;", "\"");
 
 #endif
-		}
-		#endregion
+        }
 
-	};
+        #endregion
+
+    };
 };
