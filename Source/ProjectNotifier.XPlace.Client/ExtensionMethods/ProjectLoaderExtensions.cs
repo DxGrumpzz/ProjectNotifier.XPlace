c100e3e21075cc27f6151cdeb3948edcac8e5518
Josef Lintz <yosi1.lints@gmail.com>
2019-10-02 16:58:22 +0300

    + Added a helper class for the ProjectLoader

---
+++ Source/ProjectNotifier.XPlace.Client/ExtensionMethods/ProjectLoaderExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using ProjectNotifier.XPlace.Core;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A helper class for <see cref="ProjectLoader"/> that provides extension methods
+    /// </summary>
+    public static class ProjectLoaderExtensions
+    {
+
+        /// <summary>
+        /// Loads a list of Projects and converts them to an <see cref="ObservableCollection<ProjectItemViewModel> "/>
+        /// </summary>
+        /// <param name="projectLoader"></param>
+        /// <returns></returns>
+        public async static Task<ObservableCollection<ProjectItemViewModel>> LoadProjectsAsObservableAsync(this IProjectLoader projectLoader, CancellationToken cancellationToken = default)
+        {
+            // Get a list of projects
+            var projects = (await projectLoader.LoadProjectsAsync(cancellationToken))
+            // Convert the ProjectModel to a ProjectItemViewModel
+            .Select(project => new ProjectItemViewModel()
+            {
+                ProjectModel = project,
+            });
+
+            // Return the projects as an ObservableCollection
+            return new ObservableCollection<ProjectItemViewModel>(projects);
+        }
+
+    };
+};
