fc0f7311307a0471f3d338d524ddb74ed43c42d7
Josef Lintz <yosi1.lints@gmail.com>
2019-10-03 17:28:53 +0300

    + Adde some functionality to register button

--- Source/ProjectNotifier.XPlace.Client/Viewmodels/Register Viewmodel/RegisterViewModel.cs
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Register Viewmodel/RegisterViewModel.cs
@@ -1,7 +1,8 @@
 ﻿namespace ProjectNotifier.XPlace.Client
 {
-    using ProjectNotifier.XPlace.Core;
+    using System.Diagnostics;
     using System.Threading.Tasks;
+    using ProjectNotifier.XPlace.Core;
 
 
     /// <summary>
@@ -12,12 +13,67 @@
 
         public static RegisterViewModel Intance => new RegisterViewModel()
         {
-
         };
+
+
+        #region Private fields
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private bool _registerWorking;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private bool _waitForUnloadAnimation;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private ViewAnimation _viewUnloadAnimation = ViewAnimation.SlideOutToBottom;
+
+        #endregion
+
+
+        #region Public properties
+
+        /// <summary>
+        /// A flag that indicates if the register view is working
+        /// </summary>
+        public bool RegisterWorking
+        {
+            get => _registerWorking;
+            set
+            {
+                _registerWorking = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public bool WaitForUnloadAnimation
+        {
+            get => _waitForUnloadAnimation;
+            set
+            {
+                _waitForUnloadAnimation = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public ViewAnimation ViewUnloadAnimation
+        {
+            get => _viewUnloadAnimation;
+            set
+            {
+                _viewUnloadAnimation = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
 
         #region Commands
 
         public RelayCommand GotoLoginPageCommand { get; }
+        public RelayCommand RegisterCommand { get; }
 
         #endregion
 
@@ -25,11 +81,33 @@
         public RegisterViewModel()
         {
             GotoLoginPageCommand = new RelayCommand(ExecuteGotoLoginPageCommand);
+            RegisterCommand = new RelayCommand(ExecuteRegisterCommand);
         }
+
+
+
 
         #region Command callbacks
 
+        private async Task ExecuteRegisterCommand()
+        {
+            await RunCommandAsync(async () =>
+            {
+                RegisterWorking = true;
 
+                // Change to login page
+                WaitForUnloadAnimation = true;
+                ViewUnloadAnimation = ViewAnimation.SlideOutToTop;
+
+#if DEBUG == TRUE
+                await Task.Delay(3000);
+#endif
+                DI.GetService<MainWindowViewModel>().CurrentPage = new LoginView()
+                {
+                    ViewModel = new LoginViewModel(DI.GetService<IProjectLoader>())
+                };
+            });
+        }
 
         private void ExecuteGotoLoginPageCommand()
         {
@@ -40,7 +118,7 @@
             };
         }
 
-        #endregion
+#endregion
 
     };
 };
