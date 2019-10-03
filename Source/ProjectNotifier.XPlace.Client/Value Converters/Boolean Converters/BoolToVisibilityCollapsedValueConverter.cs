ea5d555105bd794658e3b99ac12c4867ab253138
Josef Lintz <yosi1.lints@gmail.com>
2019-10-03 14:22:14 +0300

    + Added a couple of usefull boolean value convertes that will be usefull later

---
+++ Source/ProjectNotifier.XPlace.Client/Value Converters/Boolean Converters/BoolToVisibilityCollapsedValueConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BoolToVisibilityCollapsedValueConverter : BaseValueConverterT<BoolToVisibilityCollapsedValueConverter, bool, bool>
+    {
+
+        public override object ConvertT(bool value, Type targetType, bool parameter, CultureInfo culture)
+        {
+            if (parameter == true)
+                return value == true ? Visibility.Collapsed : Visibility.Visible;
+            else
+                return value == true ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    };
+};
