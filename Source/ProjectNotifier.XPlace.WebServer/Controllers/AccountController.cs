35d0ec8995abb10043360ae17bed509be373dd6e
Josef Lintz <yosi1.lints@gmail.com>
2019-10-07 16:18:01 +0300

    + Added an account controller

---
+++ Source/ProjectNotifier.XPlace.WebServer/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿namespace ProjectNotifier.XPlace.WebServer.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ProjectNotifier.XPlace.Core;
+
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        public AccountController()
+        {
+
+        }
+
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            return Content("Account index");
+        }
+
+
+        [HttpGet("index/{num}")]
+        public IActionResult Index(int num)
+        {
+            return Content($"Account index, {num}");
+        }
+
+
+        [HttpPost("Login")]
+        public IActionResult LoginAsync(LoginModel loginModel)
+        {
+            return Content($"Login: {loginModel.Username}");
+        }
+
+        [HttpPost("Register")]
+        public IActionResult LoginAsync(RegisterModel registerModel)
+        {
+            return Content($"Register: {registerModel.Username}");
+        }
+
+    }
+}
