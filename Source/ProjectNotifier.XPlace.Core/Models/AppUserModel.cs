7ab87a7007863fbcc9c40e4f458cad8c633970b8
Josef Lintz <yosi1.lints@gmail.com>
2019-10-07 17:22:51 +0300

    * Renamed AppClinetModel to AppUserModel

---
+++ Source/ProjectNotifier.XPlace.Core/Models/AppUserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using Microsoft.AspNetCore.Identity;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A model class for the app's user/client
+    /// </summary>
+    public class AppUserModel : IdentityUser
+    {
+
+        /// <summary>
+        /// The user's project preferences
+        /// </summary>
+        public IEnumerable<string> UserProjectPreferences { get; set; }
+
+    };
+};
