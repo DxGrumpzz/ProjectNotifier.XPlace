e32e766861257c6b6889396e484ac5ddec7159bf
Josef Lintz <yosi1.lints@gmail.com>
2019-10-07 16:18:11 +0300

    + Added login and registration models

---
+++ Source/ProjectNotifier.XPlace.Core/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LoginModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    };
+};
---
+++ Source/ProjectNotifier.XPlace.Core/Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RegisterModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmationPassword { get; set; }
+    };
+};
