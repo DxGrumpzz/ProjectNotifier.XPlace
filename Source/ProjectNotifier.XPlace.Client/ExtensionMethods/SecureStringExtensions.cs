a8ab2496e0cf6bee660a29391ce7df7c704e8132
Josef Lintz <yosi1.lints@gmail.com>
2019-10-10 16:28:42 +0300

    + Added a helper class for SecureString that can unsecure a SecureString

---
+++ Source/ProjectNotifier.XPlace.Client/ExtensionMethods/SecureStringExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Security;
+
+    /// <summary>
+    /// A helper class for <see cref="SecureString"/>
+    /// </summary>
+    public static class SecureStringExtensions
+    {
+
+        /// <summary>
+        /// "Converts" a <see cref="SecureString"/> to a unicode string
+        /// </summary>
+        /// <param name="secureString"> The secure string to unsecure </param>
+        /// <returns></returns>
+        public static string Unsecure(this SecureString secureString)
+        {
+            // Holds an unmanaged string in memory
+            IntPtr unmanagedString = IntPtr.Zero;
+
+            try
+            {
+                // Copy contents of secure srting to unmanagedString 
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                // Free memory
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            };
+        }
+    };
+};
