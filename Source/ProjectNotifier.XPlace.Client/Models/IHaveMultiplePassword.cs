342398a78016221c64abb5e7c6fac05594097ff8
Josef Lintz <yosi1.lints@gmail.com>
2019-10-10 16:05:43 +0300

    + Added interfaces that allow a view to pass passwords as secure strings to a comnnad

---
+++ Source/ProjectNotifier.XPlace.Client/Models/IHaveMultiplePassword.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System.Security;
+
+    /// <summary>
+    /// An interfaaace that specifes that a view has multiple password fields (password, and confirm password)
+    /// </summary>
+    public interface IHaveMultiplePassword
+    {
+        public SecureString Password { get; }
+        public SecureString ConfirmPassword { get; }
+    };
+}
---
+++ Source/ProjectNotifier.XPlace.Client/Models/IHavePassword.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System.Security;
+
+    /// <summary>
+    /// An interface that specifes that a view has a password
+    /// </summary>
+    public interface IHavePassword
+    {
+        public SecureString Password { get; }
+    };
+}
