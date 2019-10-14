9dc7f1a437d34a4f94808ef8fb2cbfc00616f1e5
Josef Lintz <yosi1.lints@gmail.com>
2019-10-14 17:31:01 +0300

    + Added WorkingIndicator as a UserControl

---
+++ Source/ProjectNotifier.XPlace.Client/Views/Wokring Indicator View/WokringIndicatorView.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System.Diagnostics;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for WokringIndicatorView.xaml
+    /// </summary>
+    public partial class WorkingIndicatorView : UserControl
+    {
+        public WorkingIndicatorView()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// A boolean flag that indicates if the working/loding indicator is shown 
+        /// </summary>
+        public bool Working
+        {
+            get => (bool)GetValue(WorkingProperty);
+            set => SetValue(WorkingProperty, value);
+        }
+
+        public static readonly DependencyProperty WorkingProperty =
+            DependencyProperty.Register(
+                nameof(Working),
+                typeof(bool),
+                typeof(WorkingIndicatorView),
+                new FrameworkPropertyMetadata(false));
+    };
+};
