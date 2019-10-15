9207b4b0cbe041d2420b02532cb17202c45cab2a
Josef Lintz <yosi1.lints@gmail.com>
2019-10-15 12:55:42 +0300

    + Added LoginResponse

---
+++ Source/ProjectNotifier.XPlace.Core/Models/API Models/Login/LoginResponseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LoginResponseModel
+    {
+        public AppUserModel UserModel { get; set; }
+
+        public IEnumerable<ProjectModel> Projects { get; set; }
+
+    };
+};
