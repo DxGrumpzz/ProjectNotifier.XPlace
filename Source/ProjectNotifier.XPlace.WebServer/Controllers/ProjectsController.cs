e0ef117944b41ca8c8c9c49331b2a8031e4ecf6b
Josef Lintz <yosi1.lints@gmail.com>
2019-10-15 13:18:19 +0300

    + Added a ProjectController, a controller that will be responsible for providing the user with up to date projects

---
+++ Source/ProjectNotifier.XPlace.WebServer/Controllers/ProjectsController.cs
@@ -0,0 +1,29 @@
+﻿namespace ProjectNotifier.XPlace.WebServer
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ProjectNotifier.XPlace.Core;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [Route("[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+
+        private readonly ProjectList _projectList;
+
+        public ProjectsController(ProjectList projectList)
+        {
+            _projectList = projectList;
+        }
+
+            
+        [HttpGet("{count?}")]
+        public IEnumerable<ProjectModel> Get(int count = 100)
+        {
+            // Get some projects
+            return _projectList.Projects.Take(count);
+        }
+
+    };
+};
