50230efe352cadcac05b47f62e524c5d37433148
Josef Lintz <yosi1.lints@gmail.com>
2019-10-15 12:54:36 +0300

    + Added ProjectList a class that is responsible for storing projects and more

---
+++ Source/ProjectNotifier.XPlace.WebServer/ProjectLoader/ProjectList.cs
@@ -0,0 +1,49 @@
+﻿namespace ProjectNotifier.XPlace.WebServer
+{
+    using ProjectNotifier.XPlace.Core;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A class that contains a list of loaded projects
+    /// </summary>
+    public class ProjectList
+    {
+        
+        #region Private properties
+
+        private readonly object _projectsLock = new object();
+
+        private IEnumerable<ProjectModel> _projects;
+
+        private readonly IProjectLoader _projectLoader;
+
+        #endregion
+
+        public IEnumerable<ProjectModel> Projects
+        {
+            get => _projects;
+            private set
+            {
+                lock (_projectsLock)
+                {
+                    _projects = value;
+                };
+            }
+        }
+
+
+        public ProjectList(IProjectLoader projectLoader)
+        {
+            _projectLoader = projectLoader;
+        }
+
+        /// <summary>
+        /// Updates the list of projects
+        /// </summary>
+        public async Task UpdateListAsync()
+        {
+            Projects = await _projectLoader.LoadProjectsAsync();
+        }
+    };
+};
