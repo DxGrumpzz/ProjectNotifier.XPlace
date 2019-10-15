6a2c45f183209e81bfe9033faef16245edddc2e2
Josef Lintz <yosi1.lints@gmail.com>
2019-10-15 12:53:15 +0300

    + Added project loader to WebServer

--- Source/ProjectNotifier.XPlace.WebServer/Startup.cs
+++ Source/ProjectNotifier.XPlace.WebServer/Startup.cs
@@ -8,15 +8,24 @@ namespace ProjectNotifier.XPlace.WebServer
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using ProjectNotifier.XPlace.Core;
+    using System;
+    using System.Collections.Generic;
 
     public class Startup
     {
+
         private readonly IConfiguration _confg;
+
+        private readonly IProjectLoader _projectLoader;
+
 
         public Startup(IConfiguration confg)
         {
             _confg = confg;
+            _projectLoader = new ProjectLoader(confg["ProjectLoderUrl"]);
         }
+
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -55,10 +64,12 @@ namespace ProjectNotifier.XPlace.WebServer
                 // Disable Endpoint routing for now, maybe I'll use it in the futures
                 config.EnableEndpointRouting = false;
             });
+            
+            services.AddSingleton(new ProjectList(_projectLoader));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDBContext dbContext)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider provider, AppDBContext dbContext)
         {
             if (env.IsDevelopment())
             {
@@ -71,6 +82,9 @@ namespace ProjectNotifier.XPlace.WebServer
             // Ensure database was created
             dbContext.Database.EnsureCreated();
 
+            // Not using async await because it will probably cause a race condition.
+            // In addition there is no point in async call here because here is where the server is being set-up
+            provider.GetService<ProjectList>().UpdateListAsync().Wait();
 
             app.UseMvc();
         }
