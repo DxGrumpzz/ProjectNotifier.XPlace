7d4125bbf437f0c97f83265eb13a829977c13242
Josef Lintz <yosi1.lints@gmail.com>
2019-10-16 13:54:19 +0300

    + Added an interface that holds hub connections

---
+++ Source/ProjectNotifier.XPlace.Client/ServerConnection/ServerConnection.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using Microsoft.AspNetCore.SignalR.Client;
+    using ProjectNotifier.XPlace.Core;
+
+    /// <summary>
+    /// An implementation of <see cref="IServerConnection"/>
+    /// </summary>
+    public class ServerConnection : IServerConnection
+    {
+        /// <summary>
+        /// The client's Projects hub connection
+        /// </summary>
+        public HubConnection ProjectsHubConnection { get; set; }
+
+    }
+}
---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/ServerConnection/IServerConnection.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using Microsoft.AspNetCore.SignalR.Client;
+
+    /// <summary>
+    /// An interface that holds a "list" of hub connection available to the client
+    /// </summary>
+    public interface IServerConnection
+    {
+        /// <summary>
+        /// The client's Projects hub connection
+        /// </summary>
+        public HubConnection ProjectsHubConnection { get; set; }
+
+    };
+}
