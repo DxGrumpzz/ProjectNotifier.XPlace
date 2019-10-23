23b8cd8d056d35da10d6c3da36e4efd5cf94b94c
Josef Lintz <yosi1.lints@gmail.com>
2019-10-23 17:37:16 +0300

    * Updated ServerConnection class implementation

--- Source/ProjectNotifier.XPlace.Client/ServerConnection/ServerConnection.cs
+++ Source/ProjectNotifier.XPlace.Client/ServerConnection/ServerConnection.cs
@@ -1,5 +1,7 @@
 ﻿namespace ProjectNotifier.XPlace.Client
 {
+    using System.Net;
+    using System.Net.Http;
     using Microsoft.AspNetCore.SignalR.Client;
     using ProjectNotifier.XPlace.Core;
 
@@ -8,10 +10,45 @@
     /// </summary>
     public class ServerConnection : IServerConnection
     {
+   
+        #region Public proeprties
+
         /// <summary>
         /// The client's Projects hub connection
         /// </summary>
         public HubConnection ProjectsHubConnection { get; set; }
 
-    }
-}
+
+        /// <summary>
+        /// The HTTP connection to the server
+        /// </summary>
+        public HttpClient Client { get; set; }
+
+
+        /// <summary>
+        /// A cookie "jar" that contains information about the connection, user authorization, and more
+        /// </summary>
+        public CookieContainer Cookies { get; private set; }
+
+
+        /// <summary>
+        /// A client handler for cookie storage
+        /// </summary>
+        public HttpClientHandler ClientHandler { get; set; }
+
+        #endregion
+
+
+        public ServerConnection()
+        {
+            Cookies = new CookieContainer();
+
+            ClientHandler = new HttpClientHandler()
+            {
+                CookieContainer = Cookies,
+            };
+
+            Client = new HttpClient(ClientHandler);
+        }
+    };
+};
