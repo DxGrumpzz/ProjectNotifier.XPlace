eaa73f3e13f21ec8643802c0303e9da00715036a
Josef Lintz <yosi1.lints@gmail.com>
2019-10-24 15:18:00 +0300

    + Added IConfig interface

---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/Config/IConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IConfig
+    {
+
+        public T GetValue<T>(string settingName);
+
+        public void SetValue<T>(string settingName, T value);
+
+    };
+}
