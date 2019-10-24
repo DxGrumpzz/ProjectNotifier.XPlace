d8220b3b2468c288b1143c27128684e05106d2ec
Josef Lintz <yosi1.lints@gmail.com>
2019-10-24 13:41:42 +0300

    + Added thread locking to logger implementations

--- Source/ProjectNotifier.XPlace.Client/Logger/ConsoleLogger/ConsoleLogger.cs
+++ Source/ProjectNotifier.XPlace.Client/Logger/ConsoleLogger/ConsoleLogger.cs
@@ -1,103 +1,106 @@
 ﻿namespace ProjectNotifier.XPlace.Client
 {
-    using ProjectNotifier.XPlace.Core;
     using System;
-	using System.Diagnostics;
-	using System.IO;
-	using System.Runtime.CompilerServices;
+    using System.IO;
+    using System.Runtime.CompilerServices;
 
-	/// <summary>
-	/// 
-	/// </summary>
-	public class ConsoleLogger : ILoggerBase
-	{
+    using ProjectNotifier.XPlace.Core;
 
-		/// <summary>
-		/// The logger's verboseness level 
-		/// </summary>
-		public LogLevel LogOutputLevel { get; set; }
+    /// <summary>
+    /// An implementation of <see cref="ILoggerBase"/> that outputs log to a console
+    /// </summary>
+    public class ConsoleLogger : ILoggerBase
+    {
+
+        private object _synchronizatingObject = new object();
 
 
-		/// <summary>
-		/// An action that will be invoked when a new log is logged 
-		/// </summary>
-		public event Action<string, LogLevel> NewLog = (message, outputLever) => { };
+        /// <summary>
+        /// The logger's verboseness level 
+        /// </summary>
+        public LogLevel LogOutputLevel { get; set; }
 
 
-		/// <summary>
-		/// Logs the message to console
-		/// </summary>
-		/// <param name="logMessage"> The message to log </param>
-		/// <param name="logLevel"> The log's output severity </param>
-		/// <param name="callerOrigin"> Where the logger was called from </param>
-		/// <param name="filePath"> The path where the log came from </param>
-		/// <param name="lineNumber"> The line number where the log was called </param>
-		public void Log(string logMessage, LogLevel logLevel = LogLevel.Normal, [CallerMemberName] string callerOrigin = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
-		{
-			// Writes the LogLevel in a format I like
-			var writeLogLevel = new Action<LogLevel, ConsoleColor>((logLevel, colour) =>
-			{
-				// Write first character
-				Console.Write('[');
-
-				// Set colour and write the log level
-				Console.ForegroundColor = colour;
-				Console.Write($"{logLevel}");
-
-				// Reset console colours and add the last bracket
-				Console.ResetColor();
-				Console.WriteLine(']');
-			});
+        /// <summary>
+        /// An action that will be invoked when a new log is logged 
+        /// </summary>
+        public event Action<string, LogLevel> NewLog = (message, outputLever) => { };
 
 
-			// Setup message format
-			switch(logLevel)
-			{
-				case LogLevel.Verbose:
-				{
-					writeLogLevel(logLevel, ConsoleColor.Yellow);
-					break;
-				};
+        /// <summary>
+        /// Logs the message to console
+        /// </summary>
+        /// <param name="logMessage"> The message to log </param>
+        /// <param name="logLevel"> The log's output severity </param>
+        /// <param name="callerOrigin"> Where the logger was called from </param>
+        /// <param name="filePath"> The path where the log came from </param>
+        /// <param name="lineNumber"> The line number where the log was called </param>
+        public void Log(string logMessage, LogLevel logLevel = LogLevel.Normal, [CallerMemberName] string callerOrigin = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            lock (_synchronizatingObject)
+            {
+                // Writes the LogLevel in a format I like
+                var writeLogLevel = new Action<LogLevel, ConsoleColor>((logLevel, colour) =>
+                {
+                    // Write first character
+                    Console.Write('[');
 
-				case LogLevel.Informative:
-				{
-					writeLogLevel(logLevel, ConsoleColor.Gray);
-					break;
-				};
+                    // Set colour and write the log level
+                    Console.ForegroundColor = colour;
+                    Console.Write($"{logLevel}");
 
-				case LogLevel.Critical:
-				{
-					writeLogLevel(logLevel, ConsoleColor.Red);
-					break;
-				};
-
-				case LogLevel.Debug:
-				{
-					writeLogLevel(logLevel, ConsoleColor.Magenta);
-					break;
-				};
-
-
-				default:
-				{
-					writeLogLevel(logLevel, ConsoleColor.Gray);
-					break;
-				};
-			};
+                    // Reset console colours and add the last bracket
+                    Console.ResetColor();
+                    Console.WriteLine(']');
+                });
 
 
+                // Setup message format
+                switch (logLevel)
+                {
+                    case LogLevel.Verbose:
+                    {
+                        writeLogLevel(logLevel, ConsoleColor.Yellow);
+                        break;
+                    };
 
-			// Setup log message format
-			string message = $"[{Path.GetFileName(filePath)} > {callerOrigin}() > Line: {lineNumber}]" +
-				$"{Environment.NewLine}{logMessage}{Environment.NewLine}";
+                    case LogLevel.Informative:
+                    {
+                        writeLogLevel(logLevel, ConsoleColor.Gray);
+                        break;
+                    };
+
+                    case LogLevel.Critical:
+                    {
+                        writeLogLevel(logLevel, ConsoleColor.Red);
+                        break;
+                    };
+
+                    case LogLevel.Debug:
+                    {
+                        writeLogLevel(logLevel, ConsoleColor.Magenta);
+                        break;
+                    };
 
 
-			// Log to the console
-			Console.WriteLine(message);
+                    default:
+                    {
+                        writeLogLevel(logLevel, ConsoleColor.Gray);
+                        break;
+                    };
+                };
 
 
-			// Invoke event
-			NewLog?.Invoke(logMessage, logLevel);
-		}
-	};
+                // Setup log message format
+                string message = $"[{Path.GetFileName(filePath)} > {callerOrigin}() > Line: {lineNumber}]" +
+                    $"{Environment.NewLine}{logMessage}{Environment.NewLine}";
+
+                // Log to the console
+                Console.WriteLine(message);
+
+                // Invoke event
+                NewLog?.Invoke(logMessage, logLevel);
+            };
+        }
+    };
 };
