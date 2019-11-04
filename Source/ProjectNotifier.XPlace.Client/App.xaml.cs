66f0158afc5be47082c4b33af45c274cfe5b17d5
Josef Lintz <yosi1.lints@gmail.com>
2019-11-04 13:38:12 +0200

    * Moved DI setup and other app setup stuff to seperate method in App.xaml.cs

--- Source/ProjectNotifier.XPlace.Client/App.xaml.cs
+++ Source/ProjectNotifier.XPlace.Client/App.xaml.cs
@@ -1,10 +1,18 @@
 ﻿namespace ProjectNotifier.XPlace.Client
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
     using System.Windows;
 
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
 
     using ProjectNotifier.XPlace.Core;
+    using ProjectNotifier.XPlace.Relational;
 
 
     /// <summary>
@@ -14,7 +22,6 @@
     {
 
 #if DEBUG == TRUE
-
         /// <summary>
         /// Creates a console window, Mainly used to display console log output
         /// </summary>
@@ -23,30 +30,42 @@
         private static extern bool AllocConsole();
 #endif
 
-        protected override void OnStartup(StartupEventArgs e)
+
+
+        protected async override void OnStartup(StartupEventArgs e)
         {
-#if DEBUG == TRUE
-            AllocConsole();
-#endif
+
 
             base.OnStartup(e);
 
 
-        
-            // Pre provider building objects.
-            
-            // Main config
-            IConfig config = new JsonConfig(AppFiles.ConfigFileName);
-            
-            // Objects that *will* be used as the main instance for DI but are necessary for DI object initializtion
-            var clientAppSettingsModel = new ClientAppSettingsModel(config);
+            // Application setup stuff, DI build and such
+            await ApplicationSetup();
+
+            // Setup MainWindow
+            (Current.MainWindow = new MainWindow(DI.GetService<MainWindowViewModel>()))
+            // Show window
+            .Show();
+        }
 
 
+        /// <summary>
+        /// Setup application necessary "modules"
+        /// </summary>
+        /// <returns></returns>
+        private async Task ApplicationSetup()
+        {
             // Add services
             ServiceCollection serviceCollection = new ServiceCollection();
 
+            // Main config
+            serviceCollection.AddSingleton<IConfig>(new JsonConfig(AppFiles.ConfigFileName));
+
 
 #if DEBUG == TRUE
+            
+            AllocConsole();
+      
             // If in Debug attach console  logger
             serviceCollection.AddSingleton<ILoggerBase, ConsoleLogger>();
 #else
@@ -54,34 +73,43 @@
 			serviceCollection.AddSingleton<ILoggerBase>(new FileLogger());
 #endif
 
-            serviceCollection.AddSingleton(clientAppSettingsModel);
+            serviceCollection.AddSingleton((provider) =>
+            new ClientAppSettingsModel(provider.GetService<IConfig>()));
 
-            serviceCollection.AddSingleton(config);
 
             serviceCollection.AddSingleton<IUIManager, UIManager>();
 
-            serviceCollection.AddSingleton(new MainWindowViewModel(clientAppSettingsModel)
-            {
-                Model = new MainWindowModel()
+
+            serviceCollection.AddSingleton((provider) =>
+                new MainWindowViewModel()
                 {
-                    Title = "XPlace Project Notifier",
-                },
-            });
+                    Model = new MainWindowModel()
+                    {
+                        Title = "XPlace Project Notifier",
+                    },
+                });
 
 
             serviceCollection.AddSingleton<IServerConnection, ServerConnection>();
 
             serviceCollection.AddSingleton<IClientCache, ClientCache>();
 
+            // Add local data stores
+            serviceCollection.AddScoped((provider) =>
+            new ClientDataStoreDBContext(
+                new DbContextOptionsBuilder<ClientDataStoreDBContext>()
+                // Use SQL lite as the storage method
+                .UseSqlite("Data Source = ProjectNotifier.XPlace.DataStore.db;")
+                .Options));
+
+            serviceCollection.AddScoped<IClientDataStore, ClientDataStore>((provider) =>
+            new ClientDataStore(provider.GetService<ClientDataStoreDBContext>()));
+
             // Build provider
             DI.SetupDI(serviceCollection.BuildServiceProvider());
 
-
-            // Setup MainWindow
-            (Current.MainWindow = new MainWindow(DI.GetService<MainWindowViewModel>()))
-            // Show window
-            .Show();
-
+            // Ensure local data store is created
+            await DI.GetService<IClientDataStore>().EnsureDataStoreCreatedAsync();
         }
     }
 }
