09b892c862b803dabf22a689a451362706a6f706
Josef Lintz <yosi1.lints@gmail.com>
2019-11-04 13:28:33 +0200

    + Added ClientDataStoreDBContext a DBContext class for the Client Data Stores

---
+++ Source/ProjectNotifier.XPlace.Relational/ClientDataStoreDBContext.cs
@@ -0,0 +1,29 @@
+﻿namespace ProjectNotifier.XPlace.Relational
+{
+    using Microsoft.EntityFrameworkCore;
+    using ProjectNotifier.XPlace.Core;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ClientDataStoreDBContext : DbContext
+    {
+
+        public DbSet<LoginCredentialsDataModel> LoginCredentials { get; set; }
+
+
+        public ClientDataStoreDBContext(DbContextOptions<ClientDataStoreDBContext> options) :
+            base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Set primary key
+            modelBuilder.Entity<LoginCredentialsDataModel>()
+                .HasKey(key => key.DataModelID);
+        }
+    };
+};
