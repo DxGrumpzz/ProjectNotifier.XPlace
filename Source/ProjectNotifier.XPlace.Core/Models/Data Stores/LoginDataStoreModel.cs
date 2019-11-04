9e710786ceb7e31954976c34b7f78ee5d41ef04b
Josef Lintz <yosi1.lints@gmail.com>
2019-11-04 13:29:09 +0200

    + Added LoginCredentialsDataModel a Model class that specifies ligin details and such

---
+++ Source/ProjectNotifier.XPlace.Core/Models/Data Stores/LoginDataStoreModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LoginCredentialsDataModel
+    {
+        public string DataModelID { get; set; }
+
+        public string Cookie { get; set; }
+
+    };
+};
