3f6257499a205ff0cb81ce35de4784a1a7628874
Josef Lintz <yosi1.lints@gmail.com>
2019-11-04 14:53:04 +0200

    + Added comments to LoginCredentialsDataModel

--- Source/ProjectNotifier.XPlace.Core/Models/Data Stores/LoginDataStoreModel.cs
+++ Source/ProjectNotifier.XPlace.Core/Models/Data Stores/LoginDataStoreModel.cs
@@ -5,12 +5,18 @@
     using System.Text;
 
     /// <summary>
-    /// 
+    /// A data models that holds login/credential information
     /// </summary>
     public class LoginCredentialsDataModel
     {
+        /// <summary>
+        /// The row's ID in the local database
+        /// </summary>
         public string DataModelID { get; set; }
 
+        /// <summary>
+        /// The cookie that allows authorization and automatic authentication
+        /// </summary>
         public string Cookie { get; set; }
 
     };
