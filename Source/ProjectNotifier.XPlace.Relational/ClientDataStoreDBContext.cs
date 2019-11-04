1286550f855b9772c52364b9be167cb6ff9f08f9
Josef Lintz <yosi1.lints@gmail.com>
2019-11-04 15:01:21 +0200

    + Added comments to ClientDataStoreDB

--- Source/ProjectNotifier.XPlace.Relational/ClientDataStoreDBContext.cs
+++ Source/ProjectNotifier.XPlace.Relational/ClientDataStoreDBContext.cs
@@ -4,11 +4,13 @@
     using ProjectNotifier.XPlace.Core;
 
     /// <summary>
-    /// 
+    /// A C# representation of a local SQL lite database
     /// </summary>
     public class ClientDataStoreDBContext : DbContext
     {
-
+        /// <summary>
+        /// A table of <see cref="LoginCredentialsDataModel"/>
+        /// </summary>
         public DbSet<LoginCredentialsDataModel> LoginCredentials { get; set; }
 
 
