607c3a4a2d03de5dc53ca2c168b7a756a4d03627
Josef Lintz <yosi1.lints@gmail.com>
2019-11-04 13:28:01 +0200

    + Added IClientDataStore an interfaces that specifies defenitions for client data storage

---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/Client Credential Store/IClientCredentialStore.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IClientDataStore
+    {
+
+        public Task EnsureDataStoreCreatedAsync();
+
+        public Task<LoginCredentialsDataModel> GetLoginCredentialsAsync();
+
+        public Task SaveLoginCredentialsAsync(LoginCredentialsDataModel loginCredentials);
+    };
+};
