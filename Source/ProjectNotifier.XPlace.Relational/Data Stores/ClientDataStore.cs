142bae1f467d9d1781b9f0a6ddbb6a3806bbfbaa
Josef Lintz <yosi1.lints@gmail.com>
2019-11-04 13:28:01 +0200

    + Added IClientDataStore an interfaces that specifies defenitions for client data storage

---
+++ Source/ProjectNotifier.XPlace.Relational/Data Stores/ClientDataStore.cs
@@ -0,0 +1,44 @@
+﻿namespace ProjectNotifier.XPlace.Relational
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using ProjectNotifier.XPlace.Core;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ClientDataStore : IClientDataStore
+    {
+
+        private readonly ClientDataStoreDBContext _clientDataStoreDBContext;
+
+
+        public ClientDataStore(ClientDataStoreDBContext clientDataStoreDBContext)
+        {
+            _clientDataStoreDBContext = clientDataStoreDBContext;
+
+        }
+
+
+        public async Task EnsureDataStoreCreatedAsync()
+        {
+            await _clientDataStoreDBContext.Database.EnsureCreatedAsync();
+        }
+
+        public async Task<LoginCredentialsDataModel> GetLoginCredentialsAsync()
+        {
+            return await Task.FromResult(_clientDataStoreDBContext.LoginCredentials.FirstOrDefault());
+        }
+
+        public async Task SaveLoginCredentialsAsync(LoginCredentialsDataModel loginCredentials)
+        {
+            _clientDataStoreDBContext.LoginCredentials.RemoveRange(_clientDataStoreDBContext.LoginCredentials);
+
+            _clientDataStoreDBContext.LoginCredentials.Add(loginCredentials);
+
+            await _clientDataStoreDBContext.SaveChangesAsync();
+        }
+    };
+};
