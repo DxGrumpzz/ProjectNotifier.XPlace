31db811d3b39d795477a6b8ef0834bf096bd1f07
Josef Lintz <yosi1.lints@gmail.com>
2019-11-06 10:46:23 +0200

    + Added ClientAppSettings a property that hold AppSettingsDataModel in ClientDataStoreDBContext

--- Source/ProjectNotifier.XPlace.Relational/ClientDataStoreDBContext.cs
+++ Source/ProjectNotifier.XPlace.Relational/ClientDataStoreDBContext.cs
@@ -13,6 +13,11 @@
         /// </summary>
         public DbSet<LoginCredentialsDataModel> LoginCredentials { get; set; }
 
+        /// <summary>
+        /// A table for the user's application settings
+        /// </summary>
+        public DbSet<AppSettingsDataModel> ClientAppSettings { get; set; }
+
 
         public ClientDataStoreDBContext(DbContextOptions<ClientDataStoreDBContext> options) :
             base(options)
@@ -20,12 +25,15 @@
 
         }
 
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Set primary key
+            // Set primary key for login credentials
             modelBuilder.Entity<LoginCredentialsDataModel>()
                 .HasKey(key => key.DataModelID);
+
+            // Set primary key for client app settings
+            modelBuilder.Entity<AppSettingsDataModel>()
+                .HasKey(key => key.RowID);
         }
     };
 };
