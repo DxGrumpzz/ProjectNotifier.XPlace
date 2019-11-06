b967cd787254ea16de53aa8c9f9b73b62621ab6e
Josef Lintz <yosi1.lints@gmail.com>
2019-11-06 16:54:33 +0200

    + Added comments

--- Source/ProjectNotifier.XPlace.Client/Viewmodels/Projects ViewModel/ProjectsPageViewModel.cs
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Projects ViewModel/ProjectsPageViewModel.cs
@@ -16,10 +16,16 @@
     /// </summary>
     public class ProjectsPageViewModel : BaseViewModel
     {
+
         #region Private fields
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private ObservableCollection<ProjectItemViewModel> _projectList;
+
+        /// <summary>
+        /// Holds the max height of the viewport
+        /// </summary>
+        private double _viewportHeight;
 
         #endregion
 
@@ -92,7 +98,6 @@
 
         #region Command Callbacks
 
-        private double _viewportHeight;
 
         /// <summary>
         /// A command that will be executed when the project list scrollviewer is scrolled
@@ -116,24 +121,26 @@
                     int projectsCount = DI.GetService<IClientCache>().ProjectListCache.Count() - ProjectList.Count;
 
                     // How big of a chuck to take out of the cached project list
-                    int chunccSize = 5 > projectsCount ? 
-                        projectsCount : 
+                    int chunccSize = 5 > projectsCount ?
+                        projectsCount :
                         5;
-
+                    
                     // Get a chunk out of the cached projectlist
-                    var projects = DI.GetService<IClientCache>().ProjectListCache
-                        .ToList()
-                        .GetRange(ProjectList.Count, chunccSize)
-                        .AsEnumerable();
-
-                    // Add the projects to this project list
-                    foreach (var project in projects)
+                    DI.GetService<IClientCache>().ProjectListCache
+                    // Convert it to a List<> to get more functionality
+                    .ToList()
+                    // Get a "Chuck" that contians the missing projects
+                    .GetRange(ProjectList.Count, chunccSize)
+                    // For every project
+                    .ForEach(project =>
                     {
+                        // Add it to the project list
                         ProjectList.Add(new ProjectItemViewModel()
                         {
                             ProjectModel = project,
                         });
-                    };
+                    });
+
 
                     // Log project list update
                     DI.Logger().Log($"Project list updated, \nDisplaying {ProjectList.Count} projects", LogLevel.Informative);
