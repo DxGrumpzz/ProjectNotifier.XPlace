a66fec92968b35a2661eb92401350e4332cc22ff
Josef Lintz <yosi1.lints@gmail.com>
2019-11-06 10:45:04 +0200

    + Added AppSettingsDataModel a model class for the user's application  settings

---
+++ Source/ProjectNotifier.XPlace.Core/Models/Data Stores/AppSettingsDataModel.cs
@@ -0,0 +1,35 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AppSettingsDataModel
+    {
+
+        /// <summary>
+        /// A unique ID string for this row
+        /// </summary>
+        public string RowID { get; set; } = Guid.NewGuid().ToString();
+
+        /// <summary>
+        /// How many projects to read/display
+        /// </summary>
+        public int ProjectsToDisplay { get; set; }
+
+        /// <summary>
+        /// How long to display the new projects notification in seconds
+        /// </summary>
+        public int KeepNotificationOpenSeconds { get; set; }
+
+        /// <summary>
+        /// Remember user credentials and auto login when app starts
+        /// </summary>
+        public bool RememberMe { get; set; }
+
+
+    };
+};
