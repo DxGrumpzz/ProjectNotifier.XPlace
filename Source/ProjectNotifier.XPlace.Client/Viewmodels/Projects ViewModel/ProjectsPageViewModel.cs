3b34432cd415e1275670bf85509396880956ff9f
Josef Lintz <yosi1.lints@gmail.com>
2019-11-06 14:44:08 +0200

    + Added "onscroll" command that adds projects if the user's scroll position is low enough. Will optimize/improve later

--- Source/ProjectNotifier.XPlace.Client/Viewmodels/Projects ViewModel/ProjectsPageViewModel.cs
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Projects ViewModel/ProjectsPageViewModel.cs
@@ -8,6 +8,8 @@
     using Microsoft.AspNetCore.SignalR.Client;
     using System.Net.Http;
     using System;
+    using System.Windows.Input;
+    using System.Windows.Controls;
 
     /// <summary>
     /// 
@@ -42,7 +44,8 @@
 
         #region Commands
 
-        public RelayCommand OpenSettingsCommand { get; }
+        public ICommand OpenSettingsCommand { get; }
+        public ICommand ProjectListScrolledCommand { get; }
 
         #endregion
 
@@ -77,6 +80,9 @@
             // Open settings page when user clicks the settings button
             OpenSettingsCommand = new RelayCommand(SettingsViewModel.OpenSettings);
 
+            ProjectListScrolledCommand = new RelayCommand<ScrollChangedEventArgs>(ExecuteProjectListScrolled);
+
+
             // Bind hub events
             DI.GetService<IServerConnection>().ProjectsHubConnection.On<IEnumerable<ProjectModel>>("ProjectListUpdated", ProjectListUpdated);
         }
@@ -86,9 +92,66 @@
 
         #region Command Callbacks
 
+        private double _viewportHeight;
 
+        /// <summary>
+        /// A command that will be executed when the project list scrollviewer is scrolled
+        /// </summary>
+        /// <param name="eventArg"></param>
+        private void ExecuteProjectListScrolled(ScrollChangedEventArgs eventArg)
+        {
+            // Get the event source/sender (the scrollviwer object)
+            ScrollViewer sender = eventArg.Source as ScrollViewer;
+
+            // Update max viewport height
+            UpdateViewportHeight(sender.ScrollableHeight);
+
+            // Check if loading the projects is even neccessary 
+            if (ProjectList.Count < DI.ClientAppSettings().ProjectsToDisplay)
+            {
+                // Check if the vertical position of the scrollbar is low enough
+                if (sender.VerticalOffset >= (_viewportHeight - 10))
+                {
+                    // The number of projects avaiable to load
+                    int projectsCount = DI.GetService<IClientCache>().ProjectListCache.Count() - ProjectList.Count;
+
+                    // How big of a chuck to take out of the cached project list
+                    int chunccSize = 5 > projectsCount ? 
+                        projectsCount : 
+                        5;
+
+                    // Get a chunk out of the cached projectlist
+                    var projects = DI.GetService<IClientCache>().ProjectListCache
+                        .ToList()
+                        .GetRange(ProjectList.Count, chunccSize)
+                        .AsEnumerable();
+
+                    // Add the projects to this project list
+                    foreach (var project in projects)
+                    {
+                        ProjectList.Add(new ProjectItemViewModel()
+                        {
+                            ProjectModel = project,
+                        });
+                    };
+
+                    // Log project list update
+                    DI.Logger().Log($"Project list updated, \nDisplaying {ProjectList.Count} projects", LogLevel.Informative);
+                };
+            };
+        }
 
         #endregion
+
+
+        #region Private helpers
+
+        private void UpdateViewportHeight(double newviewportHeight)
+        {
+            // Set viewport height if it is bigger
+            if (newviewportHeight > _viewportHeight)
+                _viewportHeight = newviewportHeight;
+        }
 
 
         private void ProjectListUpdated(IEnumerable<ProjectModel> projects)
@@ -110,10 +173,12 @@
                 {
                     ProjectModel = project,
                 })
+                .Take(DI.ClientAppSettings().ProjectsToDisplay)
                 .AsEnumerable());
 
             DI.UIManager().ShowProjectNotification(projects.Take(8));
         }
 
+        #endregion
     };
 };
