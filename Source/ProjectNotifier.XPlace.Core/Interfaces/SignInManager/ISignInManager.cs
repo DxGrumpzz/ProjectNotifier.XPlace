395fb810f2d0b4f423249a3907f9aa52d76d79f8
Josef Lintz <yosi1.lints@gmail.com>
2019-11-10 11:57:27 +0200

    + Added sign in manager implementation

---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/SignInManager/ISignInManager.cs
@@ -0,0 +1,30 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System.Net.Http;
+    using System.Security;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// An interface that specifes an implementation for a sign in manager
+    /// </summary>
+    public interface ISignInManager
+    {
+
+        /// <summary>
+        /// Sign in using regular user credentials
+        /// </summary>
+        /// <param name="username"> The user's username </param>
+        /// <param name="password"> The user's password  </param>
+        /// <returns></returns>
+        public Task<HttpResponseMessage> SignInAsync(string username, SecureString password);
+
+
+        /// <summary>
+        /// Sign in using a stored cookie 
+        /// </summary>
+        /// <param name="cookie"> The stored cookie </param>
+        /// <returns></returns>
+        public Task<HttpResponseMessage> CookieSignInAsync(string cookie);
+
+    };
+};
