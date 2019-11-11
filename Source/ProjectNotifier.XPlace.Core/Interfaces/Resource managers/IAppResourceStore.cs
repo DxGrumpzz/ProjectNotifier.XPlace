a48b1582d7e13e570f15cedc09bab97fd7ddea20
Josef Lintz <yosi1.lints@gmail.com>
2019-11-11 19:21:27 +0200

    + Added IAppResourceStore

---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/Resource managers/IAppResourceStore.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System.Collections.Generic;
+
+
+    public interface IAppResourceStore
+    {
+
+        public void AddResource(string resourceName);
+
+        public void AddResources(IEnumerable<string> resources);
+
+        public void AddResourcesFromFolder(string folderName);
+
+        public ResourceInfo GetResource(string resourceName);
+
+        public IEnumerable<ResourceInfo> GetResources(IEnumerable<string> resources);
+
+    };
+};
