e2a86feff527329d8b4671560578bdd9492967cb
Josef Lintz <yosi1.lints@gmail.com>
2019-11-11 17:31:04 +0200

    + Added settings icon view

---
+++ Source/ProjectNotifier.XPlace.Client/Views/Settings View/SettingIconView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for SettingIconView.xaml
+    /// </summary>
+    public partial class SettingIconView : UserControl
+    {
+        public SettingIconView()
+        {
+            InitializeComponent();
+        }
+    }
+}
