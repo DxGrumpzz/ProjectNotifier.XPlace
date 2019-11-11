7d328736b8e68480ff6974f2706d58386adfa7fb
Josef Lintz <yosi1.lints@gmail.com>
2019-11-11 19:21:35 +0200

    + Added IResourceLocator

---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/Resource managers/IResourceLocator.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System.Collections.Generic;
+
+
+    public interface IResourceLocator
+    {
+        public ResourceInfo FindResource(string resourceName);
+
+        public IEnumerable<ResourceInfo> FindResources(IEnumerable<string> resources);
+
+    };
+};
