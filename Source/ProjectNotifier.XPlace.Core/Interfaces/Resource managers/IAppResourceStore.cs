e4f32db0a4636507001d6f5efcca41be58460a9f
Josef Lintz <yosi1.lints@gmail.com>
2019-11-11 19:31:11 +0200

    + Added comments to resource interfaces

--- Source/ProjectNotifier.XPlace.Core/Interfaces/Resource managers/IAppResourceStore.cs
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/Resource managers/IAppResourceStore.cs
@@ -2,18 +2,39 @@
 {
     using System.Collections.Generic;
 
-
+    /// <summary>
+    /// An interface that specifies a definition for an app's resource storage
+    /// </summary>
     public interface IAppResourceStore
     {
 
+        /// <summary>
+        /// Stores a single resource
+        /// </summary>
+        /// <param name="resourceName"> The name of the resource </param>
         public void AddResource(string resourceName);
 
+        /// <summary>
+        /// Stores multiple resources 
+        /// </summary>
+        /// <param name="resources"> The namees of the resources </param>
         public void AddResources(IEnumerable<string> resources);
+
 
         public void AddResourcesFromFolder(string folderName);
 
+        /// <summary>
+        /// Fetches a single resource from the resource store
+        /// </summary>
+        /// <param name="resourceName"> The names of the resource </param>
+        /// <returns></returns>
         public ResourceInfo GetResource(string resourceName);
 
+        /// <summary>
+        /// Fetches multiple resources
+        /// </summary>
+        /// <param name="resources"> The names of the resources to retrieve </param>
+        /// <returns></returns>
         public IEnumerable<ResourceInfo> GetResources(IEnumerable<string> resources);
 
     };
--- Source/ProjectNotifier.XPlace.Core/Interfaces/Resource managers/IResourceLocator.cs
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/Resource managers/IResourceLocator.cs
@@ -2,11 +2,23 @@
 {
     using System.Collections.Generic;
 
-
+    /// <summary>
+    /// An interface that specifies an implementation of resource locator for an app's embedded resources
+    /// </summary>
     public interface IResourceLocator
     {
+        /// <summary>
+        /// Finds a single resource by name without extension (without .exe, .txt, .svg, etc...)
+        /// </summary>
+        /// <param name="resourceName"> The name of the resource without extension </param>
+        /// <returns></returns>
         public ResourceInfo FindResource(string resourceName);
 
+        /// <summary>
+        /// Finds multiple resources by name
+        /// </summary>
+        /// <param name="resources"> The names of the resources </param>
+        /// <returns></returns>
         public IEnumerable<ResourceInfo> FindResources(IEnumerable<string> resources);
 
     };
