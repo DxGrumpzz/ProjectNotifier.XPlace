8c160f7510b68de5886020221dc1cd07b02f5a77
Josef Lintz <yosi1.lints@gmail.com>
2019-11-11 17:38:47 +0200

    + Added an enum that specifies setting icons

---
+++ Source/ProjectNotifier.XPlace.Core/Models/Enums/SettingIcon.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// An enum that specifies setting icons
+    /// </summary>
+    public enum SettingIcon
+    {
+        
+        /// <summary>
+        /// Settings related to the suer
+        /// </summary>
+        UserSettings = 0,
+
+        /// <summary>
+        /// Settings related to the application
+        /// </summary>
+        ApplicationSettings = 0,
+
+    };
+};
