ed7ec800fdc4ab1382b84b5cda36579678223beb
Josef Lintz <yosi1.lints@gmail.com>
2019-11-11 19:07:01 +0200

    + Added ResourceInfo, A class that contains information about an embedded resource

---
+++ Source/ProjectNotifier.XPlace.Client/ResourceLocator/ResourceInfo.cs
@@ -0,0 +1,55 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using System.IO;
+
+    /// <summary>
+    /// A class that contains information about an embedded resource
+    /// </summary>
+    public class ResourceInfo
+    {
+
+        #region Public properties
+
+        /// <summary>
+        /// The name of the resource
+        /// </summary>
+        public string ResourceName { get; set; }
+
+        /// <summary>
+        /// The resource's data stream
+        /// </summary>
+        public Stream ResourceStream { get; set; }
+
+        /// <summary>
+        /// The resource fullname/path
+        /// </summary>
+        public string ResourceFullname { get; set; }
+
+        #endregion
+
+
+        #region Public methods
+
+        /// <summary>
+        /// Returns the Resource's data Stream to a byte array
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ToByteArray()
+        {
+            // Validation
+            if (ResourceStream is null || ResourceStream.Length == 0)
+                return null;
+            
+            // Holds the Resource's data stream 
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                // Copy resource stream data to the memory stream
+                ResourceStream.CopyTo(memoryStream);
+
+                return memoryStream.ToArray();
+            };
+        }
+
+        #endregion
+    };
+};
