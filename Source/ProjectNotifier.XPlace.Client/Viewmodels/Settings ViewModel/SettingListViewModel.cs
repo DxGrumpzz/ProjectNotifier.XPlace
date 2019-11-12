40f50987630a1579aa52bcbf6ad91f0973bee396
Josef Lintz <yosi1.lints@gmail.com>
2019-11-12 13:14:16 +0200

    + Added SettingsListViewModel

---
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/SettingListViewModel.cs
@@ -0,0 +1,48 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using ProjectNotifier.XPlace.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Text;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class SettingsListViewModel
+    {
+
+        public List<SettingIconViewModel> SettingIcons { get; set; }
+
+
+        public SettingsListViewModel()
+        {
+            SettingIcons = new List<SettingIconViewModel>()
+            {
+                new SettingIconViewModel()
+                {
+                    Description = "הגדרות אפליקצייה",
+                    Icon = SettingIcon.ApplicationSettings,
+
+                    GotoSettingCommand = new RelayCommand(() =>
+                    {
+                        var s = DI.GetService<MainWindowViewModel>().CurrentPage.ViewModel;
+
+                        ((ProjectsPageViewModel)s).SettingsViewModel.CurrentSettingsPage = new AppSettingsView();
+                    }),
+                },
+
+                new SettingIconViewModel()
+                {
+                    Description = "הגדרות משתמש",
+                    Icon = SettingIcon.UserSettings,
+
+                    GotoSettingCommand = new RelayCommand(() =>
+                    {
+
+                    }),
+                },
+            };
+        }
+    };
+};
