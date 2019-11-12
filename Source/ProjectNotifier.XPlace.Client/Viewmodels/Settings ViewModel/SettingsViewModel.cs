f4b3e02628f2f67e9ea6319d9b3dce781c924bbb
Josef Lintz <yosi1.lints@gmail.com>
2019-11-12 17:13:09 +0200

    - Remove unnecessary code from SettingsViewModel

--- Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/SettingsViewModel.cs
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/SettingsViewModel.cs
@@ -1,9 +1,8 @@
 ﻿namespace ProjectNotifier.XPlace.Client
 {
     using ProjectNotifier.XPlace.Core;
-    using System;
+
     using System.Threading.Tasks;
-    using System.Windows.Controls;
 
     /// <summary>
     /// 
@@ -19,33 +18,12 @@
 
         private bool _isOpening;
 
-        private bool _rememberMe;
-
-        /// <summary>
-        /// Application settings
-        /// </summary>
-        private AppSettingsDataModel _clientAppSettingsModel;
-
-
         private BaseView _currentSettingsPage;
 
         #endregion
 
 
         #region Public properties
-
-
-
-        /// <summary>
-        /// How many projects are currently being disaplyed
-        /// </summary>
-        public TextEntryViewModel<int> ProjectCountSetting { get; private set; }
-
-        /// <summary>
-        /// How long the new projects notification will be displayed for
-        /// </summary>
-        public TextEntryViewModel<int> NotificationDispalySecondsSetting { get; private set; }
-
 
         /// <summary>
         /// A boolean flag that indicates if the settings saved notification is open
@@ -61,35 +39,6 @@
         }
 
         /// <summary>
-        /// A boolean flag that indiactes if the user will automatically login next time the app opens
-        /// </summary>
-        public bool RememberMe
-        {
-            get => _rememberMe;
-            set
-            {
-                if (_rememberMe != value)
-                {
-                    _rememberMe = value;
-
-                    OnPropertyChanged();
-
-                    var settings = DI.GetService<IClientDataStore>().GetClientAppSettings();
-
-                    settings.RememberMe = value;
-                    // Update config value
-                    //_clientAppSettingsModel.RememberMe = value;
-
-                    DI.GetService<IClientDataStore>().SaveClientAppSettings();
-
-                    // Show saved notification
-                    Task.Run(ShowSavedNOtificationAsync);
-                };
-            }
-        }
-
-
-        /// <summary>
         /// A boolean flag that indicates if this control is open or in view
         /// </summary>
         public bool IsOpen
@@ -102,13 +51,9 @@
             }
         }
 
-
-        #endregion
-
-
-        public RelayCommand CloseSettingsCommand { get; }
-
-
+        /// <summary>
+        /// Currently displayed settings page view
+        /// </summary>
         public BaseView CurrentSettingsPage
         {
             get => _currentSettingsPage;
@@ -119,6 +64,11 @@
             }
         }
 
+        #endregion
+
+
+        public RelayCommand CloseSettingsCommand { get; }
+
 
         public SettingsViewModel()
         {
@@ -126,111 +76,6 @@
             {
                 ViewModel = new SettingsListViewModel()
             };
-
-
-            _clientAppSettingsModel = DI.ClientAppSettings();
-
-
-            ProjectCountSetting = new TextEntryViewModel<int>(_clientAppSettingsModel.ProjectsToDisplay)
-            {
-                IsNumericOnly = true,
-
-                MaxLength = 3,
-            };
-
-            NotificationDispalySecondsSetting = new TextEntryViewModel<int>(_clientAppSettingsModel.KeepNotificationOpenSeconds)
-            {
-                IsNumericOnly = true,
-
-                MaxLength = 2,
-            };
-
-            RememberMe = _clientAppSettingsModel.RememberMe;
-
-
-            // Bind events
-            ProjectCountSetting.ValueValidationAction += new Func<TextEntryViewModel<int>, bool>(setting =>
-            {
-                // If value didn't change don't update
-                if (setting.Value == setting.PreviousValue)
-                {
-                    return false;
-                };
-
-                // Boundry validation
-                if (setting.Value > 100)
-                {
-                    setting.Value = 100;
-                    return false;
-                }
-                else if (setting.Value < 25)
-                {
-                    setting.Value = 25;
-                    return false;
-                };
-
-                return true;
-            });
-
-            ProjectCountSetting.SaveChangesAction += new Action<TextEntryViewModel<int>>(async (setting) =>
-            {
-                DI.Logger().Log($"User changed {nameof(AppSettingsDataModel.ProjectsToDisplay)} setting to {setting.Value}", LogLevel.Informative);
-
-                // Update value 
-                var settings = await DI.GetService<IClientDataStore>().GetClientAppSettingsAsync();
-
-                settings.ProjectsToDisplay = setting.Value;
-
-                await DI.GetService<IClientDataStore>().SaveClientAppSettingsAsync();
-
-                // Update config value
-                //DI.GetService<JsonConfigManager>().WriteSetting(nameof(ClientAppSettingsModel.ProjectsToDisplay), setting.Value);
-
-                // Show settings saved notification
-                await ShowSavedNOtificationAsync();
-            });
-
-            NotificationDispalySecondsSetting.ValueValidationAction += new Func<TextEntryViewModel<int>, bool>(setting =>
-            {
-                // If value didn't change don't update
-                if (setting.Value == setting.PreviousValue)
-                {
-                    return false;
-                };
-
-                // Boundry validation
-                if (setting.Value > 10)
-                {
-                    setting.Value = 10;
-                    return false;
-                }
-                else if (setting.Value < 3)
-                {
-                    setting.Value = 3;
-                    return false;
-                };
-
-                return true;
-            });
-
-            NotificationDispalySecondsSetting.SaveChangesAction += new Action<TextEntryViewModel<int>>(async (setting) =>
-            {
-                DI.Logger().Log($"User changed {nameof(AppSettingsDataModel.KeepNotificationOpenSeconds)} setting to {setting.Value}", LogLevel.Informative);
-
-                // Update value 
-                var settings = await DI.GetService<IClientDataStore>().GetClientAppSettingsAsync();
-
-                settings.KeepNotificationOpenSeconds = setting.Value;
-
-                await DI.GetService<IClientDataStore>().SaveClientAppSettingsAsync();
-
-                // Update config value
-                //DI.GetService<JsonConfigManager>().WriteSetting(nameof(ClientAppSettingsModel.KeepNotificationOpenSeconds), setting.Value);
-
-                // Show settings saved notification
-                await ShowSavedNOtificationAsync();
-            });
-
 
             CloseSettingsCommand = new RelayCommand(ExecuteCloseSettingsCommand);
         }
@@ -278,7 +123,7 @@
         /// <summary>
         /// Animates the settings saved notification
         /// </summary>
-        private async Task ShowSavedNOtificationAsync()
+        public async Task ShowSavedNotificationAsync()
         {
             if (SavedNotificationOpen == true)
                 return;
