074c62ce161dfa56752860a0391033e6d7ebcd81
Josef Lintz <yosi1.lints@gmail.com>
2019-11-12 13:12:42 +0200

    + Added AppSettingsView a very VERY basic view for the Application's settings

---
+++ Source/ProjectNotifier.XPlace.Client/Views/Settings View/Setting Pages/AppSettingsView.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using ProjectNotifier.XPlace.Core;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for AppSettingsPage.xaml
+    /// </summary>
+    public partial class AppSettingsView : UserControl
+    {
+        public AppSettingsView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var s = DI.GetService<MainWindowViewModel>().CurrentPage.ViewModel;
+
+            ((ProjectsPageViewModel)s).SettingsViewModel.CurrentSettingsPage = new SettingsListView()
+            {
+                DataContext = new SettingsListViewModel(),
+            };
+        }
+    };
+};
