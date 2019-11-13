6432684c56a26bae58f753a073710eb7b81b7fa4
Josef Lintz <yosi1.lints@gmail.com>
2019-11-13 12:55:17 +0200

    - Removed unnecessary event from AppSettingsView

--- Source/ProjectNotifier.XPlace.Client/Views/Settings View/Setting Pages/AppSettingsView.xaml.cs
+++ Source/ProjectNotifier.XPlace.Client/Views/Settings View/Setting Pages/AppSettingsView.xaml.cs
@@ -14,15 +14,5 @@
 
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            var s = DI.GetService<MainWindowViewModel>().CurrentPage.ViewModel;
-
-            ((ProjectsPageViewModel)s).SettingsViewModel.CurrentSettingsPage = new SettingsListView()
-            {
-                DataContext = new SettingsListViewModel(),
-            };
-        }
     };
 };
