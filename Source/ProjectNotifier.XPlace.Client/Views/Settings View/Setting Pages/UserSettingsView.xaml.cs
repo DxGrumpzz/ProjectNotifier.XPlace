01d70dea47f8d4d623e1bce516e141fb34771a7c
Josef Lintz <yosi1.lints@gmail.com>
2019-11-13 16:29:00 +0200

    - Removed unnecessary event from UserSettingsvIew

--- Source/ProjectNotifier.XPlace.Client/Views/Settings View/Setting Pages/UserSettingsView.xaml.cs
+++ Source/ProjectNotifier.XPlace.Client/Views/Settings View/Setting Pages/UserSettingsView.xaml.cs
@@ -1,18 +1,8 @@
 ﻿namespace ProjectNotifier.XPlace.Client
 {
     using ProjectNotifier.XPlace.Core;
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+
     using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
 
     /// <summary>
     /// Interaction logic for UserSettingsView.xaml
@@ -24,16 +14,6 @@
             ViewLoadAnimation = ViewAnimation.FadeIn;
 
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var s = DI.GetService<MainWindowViewModel>().CurrentPage.ViewModel;
-
-            ((ProjectsPageViewModel)s).SettingsViewModel.CurrentSettingsPage = new SettingsListView()
-            {
-                DataContext = new SettingsListViewModel(),
-            };
         }
     };
 };
