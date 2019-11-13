8fb5e2e7605ed3003484167f970b2a704203f121
Josef Lintz <yosi1.lints@gmail.com>
2019-11-13 18:00:49 +0200

    + Added UserSettingsViewModel

---
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/Settings Pages ViewModels/UserSettingsViewModel.cs
@@ -0,0 +1,48 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using ProjectNotifier.XPlace.Core;
+
+    using System.Collections.ObjectModel;
+    using System.Diagnostics;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class UserSettingsViewModel : BaseViewModel
+    {
+
+        #region Private fields
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private ObservableCollection<ProjectTypes> _projectPreferences;
+
+        #endregion
+
+
+        #region Public properties
+
+        /// <summary>
+        /// The user's project preferences
+        /// </summary>
+        public ObservableCollection<ProjectTypes> ProjectPreferences
+        {
+            get => _projectPreferences;
+            set
+            {
+                _projectPreferences = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+
+        public UserSettingsViewModel()
+        {
+
+        }
+
+
+
+    };
+};
