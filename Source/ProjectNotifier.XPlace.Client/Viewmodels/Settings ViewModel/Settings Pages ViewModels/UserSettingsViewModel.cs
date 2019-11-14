1763ac5ac7e7abc8c52be104360a19f177399188
Josef Lintz <yosi1.lints@gmail.com>
2019-11-14 15:15:51 +0200

    + Added ShowProjectPreferencesMenu Command to UserSettingsViewModel

--- Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/Settings Pages ViewModels/UserSettingsViewModel.cs
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/Settings Pages ViewModels/UserSettingsViewModel.cs
@@ -1,15 +1,30 @@
 ﻿namespace ProjectNotifier.XPlace.Client
 {
     using ProjectNotifier.XPlace.Core;
-
+    using System;
     using System.Collections.ObjectModel;
     using System.Diagnostics;
+    using System.Windows.Input;
 
     /// <summary>
     ///
     /// </summary>
     public class UserSettingsViewModel : BaseViewModel
     {
+
+        public static UserSettingsViewModel DesginInstance => new UserSettingsViewModel()
+        {
+
+            ProjectPreferences = new ObservableCollection<ProjectTypes>()
+            {
+                ProjectTypes.Administration,
+                ProjectTypes.ArchitectureAndInteriorDesign,
+                ProjectTypes.CoachingAndTraining,
+                ProjectTypes.Executives,
+            },
+
+        };
+
 
         #region Private fields
 
@@ -37,12 +52,22 @@
         #endregion
 
 
+        #region Commands
+
+        public ICommand ShowProjectPreferencesMenuCommand { get; }
+
+        #endregion
+
         public UserSettingsViewModel()
         {
-
+            ShowProjectPreferencesMenuCommand = new RelayCommand(ExecuteShowProjectPreferencesMenuCommand);
         }
 
+        private void ExecuteShowProjectPreferencesMenuCommand()
+        {
+            
 
 
+        }
     };
 };
