a43062e1471661d9ceadf12ad35332430c2b5739
Josef Lintz <yosi1.lints@gmail.com>
2019-11-14 20:45:10 +0200

    + Added propper preffered project type loading in ProjectPreferenceSelectionMenuViewModel

--- Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/Settings Pages ViewModels/ProjectPreferenceSelectionMenuViewModel.cs
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/Settings Pages ViewModels/ProjectPreferenceSelectionMenuViewModel.cs
@@ -3,6 +3,7 @@
     using ProjectNotifier.XPlace.Core;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Windows.Input;
 
 
@@ -61,17 +62,39 @@
         /// <param name="userProjects"> The user's prefered project types </param>
         public ProjectPreferenceSelectionMenuViewModel(IEnumerable<ProjectTypes> userProjects)
         {
-            AvailableProjectTypes = new List<ProjectTypes>(userProjects);
-
+            // Load available projects
+            AvailableProjectTypes = new List<ProjectTypes>(GetNeccessaryProjects(userProjects));
 
 
             CloseMenuCommand = new RelayCommand(ExecuteCloseMenuCommand);
         }
 
+        
 
         private void ExecuteCloseMenuCommand()
         {
             IsMenuOpen = false;
         }
+
+
+        #region Private helpers
+
+        /// <summary>
+        /// Retrieves a list of preffered project types depending on the user's already existing list
+        /// </summary>
+        /// <param name="userProjects"> The list of the user's preffered projects </param>
+        private IEnumerable<ProjectTypes> GetNeccessaryProjects(IEnumerable<ProjectTypes> userProjects)
+        {
+            // Get a array of integers representing each value in ProjectTypes
+            var projectTypeNumericValues = ((int[])Enum.GetValues(typeof(ProjectTypes)))
+                .Select(projectTypeValue => (ProjectTypes)projectTypeValue);
+
+            // Get only the project types that aren't in the user's projects list
+            var differences = projectTypeNumericValues.Except(userProjects);
+
+            return differences;
+        }
+
+        #endregion
     };
 };
