49c3f2323f17cff90e1393ad32e06ad0302fa66f
Josef Lintz <yosi1.lints@gmail.com>
2019-11-17 14:33:54 +0200

    + Added UserProjectPreferenceItemViewModel

---
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/Settings Pages ViewModels/User settings viewmodels/UserProjectPreferenceItemViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using ProjectNotifier.XPlace.Core;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// A viewmodel for a user's alredy preffered project type 
+    /// </summary>
+    public class UserProjectPreferenceItemViewModel : BaseViewModel
+    {
+
+        #region Public properties
+
+        public ProjectTypes ProjectType { get; set; }
+
+        #endregion
+
+
+        #region Commands
+
+        public ICommand RemovePreferenceCommand { get; }
+
+        #endregion
+
+
+        public UserProjectPreferenceItemViewModel()
+        {
+            RemovePreferenceCommand = new RelayCommand(ExecuteRemovePreference);
+        }
+
+
+        private void ExecuteRemovePreference()
+        {
+            
+        }
+    };
+};
