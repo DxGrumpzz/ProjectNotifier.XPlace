3db7ba7f23244018def01fa0a2517c525d888626
Josef Lintz <yosi1.lints@gmail.com>
2019-11-17 13:05:43 +0200

    + Added a viewmodel for project menu item

---
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/Settings Pages ViewModels/ProjectPreference Menu/ProjectPreferenceMenuItemViewModel.cs
@@ -0,0 +1,45 @@
+﻿namespace ProjectNotifier.XPlace.Client
+{
+    using ProjectNotifier.XPlace.Core;
+
+    using System.Windows.Input;
+
+    /// <summary>
+    /// A viewmodel class for a project preference type 
+    /// </summary>
+    public class ProjectPreferenceMenuItemViewModel : BaseViewModel
+    {
+
+        #region Public properties
+
+        /// <summary>
+        /// The project types that the user can select 
+        /// </summary>
+        public ProjectTypes ProjectTypes { get; set; }
+
+        #endregion
+
+
+        #region Commands
+
+        public ICommand AddProjectTypeCommand { get; }
+
+        #endregion
+
+
+        public ProjectPreferenceMenuItemViewModel()
+        {
+            AddProjectTypeCommand = new RelayCommand(ExecuteAddProjectTypeCommand);
+        }
+
+
+        #region Command callbacks
+
+        private void ExecuteAddProjectTypeCommand()
+        {
+
+        } 
+        #endregion
+
+    };
+};
