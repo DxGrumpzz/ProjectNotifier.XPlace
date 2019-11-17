1bf30bed75d736f5d73245345b25551a0f1d271b
Josef Lintz <yosi1.lints@gmail.com>
2019-11-17 14:48:25 +0200

    + Added removing project preferences

--- Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/Settings Pages ViewModels/User settings viewmodels/UserProjectPreferenceItemViewModel.cs
+++ Source/ProjectNotifier.XPlace.Client/Viewmodels/Settings ViewModel/Settings Pages ViewModels/User settings viewmodels/UserProjectPreferenceItemViewModel.cs
@@ -1,7 +1,9 @@
 ﻿namespace ProjectNotifier.XPlace.Client
 {
     using ProjectNotifier.XPlace.Core;
+    using System.Linq;
     using System.Windows.Input;
+
 
     /// <summary>
     /// A viewmodel for a user's alredy preffered project type 
@@ -25,13 +27,44 @@
 
         public UserProjectPreferenceItemViewModel()
         {
-            RemovePreferenceCommand = new RelayCommand(ExecuteRemovePreference);
+            RemovePreferenceCommand = new RelayCommand(ExecuteRemovePreferenceCommand);
         }
 
 
-        private void ExecuteRemovePreference()
+        private void ExecuteRemovePreferenceCommand()
         {
-            
+            // TODO: improve later 
+
+            var userProfile = DI.GetService<IClientDataStore>().GetUserProfile();
+
+            // Get user settings viewmodel
+            var vm = ((UserSettingsViewModel)DI.GetService<ProjectsPageViewModel>().SettingsViewModel.CurrentSettingsPage.ViewModel);
+
+            // Add this item to user preffered project list
+            vm.ProjectPreferences.Remove(this);
+
+            // Remove this item from project preference selection menu
+            vm.ProjectPreferenceSelectionMenuViewModel.AvailableProjectTypes.Add(new ProjectPreferenceMenuItemViewModel()
+            {
+                ProjectType = ProjectType,
+            });
+
+
+            // Update UserProfile project list
+            userProfile.UserProjectPreferences = vm.ProjectPreferences
+                .Select(projectType => new UserProjectPreference()
+                {
+                    ProjectType = projectType.ProjectType,
+                    User = userProfile,
+                })
+                .AsEnumerable();
+
+
+            // If user added last project type
+            //if (vm.ProjectPreferences.Count == 0)
+            //{
+
+            //};
         }
     };
 };
