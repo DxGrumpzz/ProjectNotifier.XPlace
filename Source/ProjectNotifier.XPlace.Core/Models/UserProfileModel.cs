1c828a8002fad8a6759617da64083317140c85b3
Josef Lintz <yosi1.lints@gmail.com>
2019-11-20 19:33:43 +0200

    + Added UserProfileModel

---
+++ Source/ProjectNotifier.XPlace.Core/Models/UserProfileModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// A model class that represetnts a user's profile data
+    /// </summary>
+    public class UserProfileModel
+    {
+        /// <summary>
+        /// The user's username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The user's unique ID
+        /// </summary>
+        public string UserID { get; set; }
+
+        /// <summary>
+        /// The user's project preferences list
+        /// </summary>
+        public IEnumerable<ProjectTypes> UserProjectPreferences { get; set; }
+    };
+};
