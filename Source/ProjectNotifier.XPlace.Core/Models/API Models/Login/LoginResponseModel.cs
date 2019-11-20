b30d8adf8cd00fa0057f8a7817a72eed5f7be688
Josef Lintz <yosi1.lints@gmail.com>
2019-11-20 19:34:16 +0200

    - Removed LoginResponse properties

--- Source/ProjectNotifier.XPlace.Core/Models/API Models/Login/LoginResponseModel.cs
+++ Source/ProjectNotifier.XPlace.Core/Models/API Models/Login/LoginResponseModel.cs
@@ -7,14 +7,8 @@
     /// </summary>
     public class LoginResponseModel
     {
-        //public AppUserModel UserModel { get; set; }
 
-        public string Username { get; set; }
-
-        public string UserID { get; set; }
-
-        public IEnumerable<ProjectTypes> UserProjectPreferences { get; set; }
-
+        public UserProfileModel UserProfile { get; set; }
 
         public IEnumerable<ProjectModel> Projects { get; set; }
 
