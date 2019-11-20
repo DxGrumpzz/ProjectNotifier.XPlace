10d0eee5bfddb7ca10e3000172715c508ffc2323
Josef Lintz <yosi1.lints@gmail.com>
2019-11-20 19:20:10 +0200

    - Removed AppUserModel property from LoginResponse and added sperate properties

--- Source/ProjectNotifier.XPlace.Core/Models/API Models/Login/LoginResponseModel.cs
+++ Source/ProjectNotifier.XPlace.Core/Models/API Models/Login/LoginResponseModel.cs
@@ -7,7 +7,14 @@
     /// </summary>
     public class LoginResponseModel
     {
-        public AppUserModel UserModel { get; set; }
+        //public AppUserModel UserModel { get; set; }
+
+        public string Username { get; set; }
+
+        public string UserID { get; set; }
+
+        public IEnumerable<ProjectTypes> UserProjectPreferences { get; set; }
+
 
         public IEnumerable<ProjectModel> Projects { get; set; }
 
