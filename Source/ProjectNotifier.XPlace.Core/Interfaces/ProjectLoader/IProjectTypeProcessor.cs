ff36e5c0873267d0084f988a526d59fcd03bfcd1
Josef Lintz <yosi1.lints@gmail.com>
2019-11-25 13:17:10 +0200

    + Added IProjectTypeProcessor interface

---
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/ProjectLoader/IProjectTypeProcessor.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// An interface for a processor of text that is used to determine the type of a project, Whether it is development, desgin, and others...
+    /// </summary>
+    public interface IProjectTypeProcessor
+    {
+        
+        /// <summary>
+        /// Takes a <see cref="ProjectModel"/> and finds it's fitting types
+        /// </summary>
+        /// <param name="project"> The project to check </param>
+        /// <returns> Returns an <see cref="IEnumerable{T}"/> of <see cref="ProjectType"/> </returns>
+        public IEnumerable<ProjectType> GetProjectType(in ProjectModel project);
+
+    };
+};
