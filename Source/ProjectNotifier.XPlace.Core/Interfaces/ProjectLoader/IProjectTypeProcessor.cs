d45e1026547c2690f2c53f21f0fa93361dc97a97
Josef Lintz <yosi1.lints@gmail.com>
2019-11-26 15:23:47 +0200

    - Remove in argument from GetProjectType parameter

--- Source/ProjectNotifier.XPlace.Core/Interfaces/ProjectLoader/IProjectTypeProcessor.cs
+++ Source/ProjectNotifier.XPlace.Core/Interfaces/ProjectLoader/IProjectTypeProcessor.cs
@@ -13,7 +13,7 @@
         /// </summary>
         /// <param name="project"> The project to check </param>
         /// <returns> Returns an <see cref="IEnumerable{T}"/> of <see cref="ProjectType"/> </returns>
-        public IEnumerable<ProjectType> GetProjectType(in ProjectModel project);
+        public IEnumerable<ProjectType> GetProjectType(ProjectModel project);
 
     };
 };
