930d5d054cac585abdc7a6a8663191c1a274aad2
Josef Lintz <yosi1.lints@gmail.com>
2019-12-01 11:48:12 +0200

    + Added ApiRoutes a clas that localizes ApiRoutes

---
+++ Source/ProjectNotifier.XPlace.Core/Localization/ApiRoutes.cs
@@ -0,0 +1,48 @@
+﻿namespace ProjectNotifier.XPlace.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// A static class containing routes to API controllers and actions
+    /// </summary>
+    public static class ApiRoutes
+    {
+        
+        #region Account Controller
+
+        /// <summary>
+        /// An api route for the account contoller
+        /// </summary>
+        public const string API_ACCOUNT_CONTROLLER = "Account";
+
+        /// <summary>
+        /// An api route for the Login action in account contoller
+        /// </summary>
+        public const string API_ACCOUNT_CONTROLLER_LOGIN = "Login";
+
+        /// <summary>
+        /// An api route for the register action in account contoller
+        /// </summary>
+        public const string API_ACCOUNT_CONTROLLER_REGISTER = "Register";
+
+        #endregion
+
+
+        #region Profile Controller
+
+        /// <summary>
+        /// An api route for the profile contoller
+        /// </summary>
+        public const string API_PROFILE_CONTROLLER = "Profile";
+
+        /// <summary>
+        /// An api route for the UpdateUserPreferences Action in the profile contoller
+        /// </summary>
+        public const string API_PROFILE_UPDATE_USER_PREFERENCES = "UpdateUserPreferences";
+
+        #endregion
+
+    };
+};
